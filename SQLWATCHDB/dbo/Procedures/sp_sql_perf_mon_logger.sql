bd974a60e8f1d33cdcb0d9c6513cb13d5a5bda31
Marcin Gminski <marcingminski@gmail.com>
2019-02-01 15:18:23 +0000

    performance tweak to exclude wait stats with zero waits.

--- SQLWATCHDB/dbo/Procedures/sp_sql_perf_mon_logger.sql
+++ SQLWATCHDB/dbo/Procedures/sp_sql_perf_mon_logger.sql
@@ -286,7 +286,8 @@ declare @sql nvarchar(4000)
 		--------------------------------------------------------------------------------------------------------------
 		insert into [dbo].[sql_perf_mon_wait_stats]
 		select [wait_type], [waiting_tasks_count], [wait_time_ms],[max_wait_time_ms], [signal_wait_time_ms], [snapshot_time]=@date_snapshot_current, 1
-		from sys.dm_os_wait_stats;
+		from sys.dm_os_wait_stats
+		where waiting_tasks_count + wait_time_ms + max_wait_time_ms + signal_wait_time_ms > 0
 
 		--------------------------------------------------------------------------------------------------------------
 		-- XE waits
