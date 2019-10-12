702b655041efe98abbf62f7fe6afaf879cc43d8f
Marcin Gminski <marcingminski@gmail.com>
2019-10-12 23:46:52 +0100

    fix for implicit conversion warning

--- SQLWATCHDB/dbo/Tables/sqlwatch_meta_agent_job.sql
+++ SQLWATCHDB/dbo/Tables/sqlwatch_meta_agent_job.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[sqlwatch_meta_agent_job]
 (
 	[sql_instance] nvarchar(25) not null,
-	[job_name] sysname not null,
+	[job_name] nvarchar(128) not null,
 	[job_create_date] datetime not null,
 	[sqlwatch_job_id] smallint identity (-32768,1),
 	constraint pk_sqlwatch_meta_agent_job primary key (
--- SQLWATCHDB/dbo/Tables/sqlwatch_meta_agent_job_step.sql
+++ SQLWATCHDB/dbo/Tables/sqlwatch_meta_agent_job_step.sql
@@ -2,7 +2,7 @@
 (
 	[sql_instance] nvarchar(25) not null,
 	[sqlwatch_job_id] smallint not null,
-	[step_name] sysname not null,
+	[step_name] nvarchar(128) not null,
 	[sqlwatch_job_step_id] smallint not null identity (-32768,1),
 	constraint pk_sqlwatch_meta_agent_job_step primary key (
 		[sql_instance], [sqlwatch_job_id], [sqlwatch_job_step_id]
