843c6b49d7f3b120395eb3f18d9efa144cc4295b
Marcin Gminski <marcingminski@gmail.com>
2019-10-13 01:06:34 +0100

    amended to follow normalised objects

--- SQLWATCHDB/fix_trim_perf_counter.sql
+++ SQLWATCHDB/fix_trim_perf_counter.sql
@@ -1,13 +1,13 @@
 ﻿while (select count(*) from [dbo].[sqlwatch_logger_perf_os_performance_counters] 
-	where DATALENGTH(object_name) in ( 128, 256 )
+	where DATALENGTH([performance_counter_id]) in ( 128, 256 )
 	) > 0
 		begin
 			with cte_update as (
 				select top 10000 * from  [dbo].[sqlwatch_logger_perf_os_performance_counters] 
-				where DATALENGTH(object_name) in ( 128, 256 )
+				where DATALENGTH([performance_counter_id]) in ( 128, 256 )
 			)
 			  update cte_update
-				set [object_name] = rtrim([object_name])
+				set [performance_counter_id] = rtrim([performance_counter_id])
 				, [instance_name] = rtrim([instance_name])
 				, [counter_name] = rtrim([counter_name])
 		end
