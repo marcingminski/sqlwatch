536314328c3a7ab879dd7a235989b708c6fbe223
Marcin Gminski <marcingminski@gmail.com>
2019-10-14 15:47:55 +0100

    purge deleted items based on time

---
+++ SQLWATCHDB/dbo/Procedures/usp_sqlwatch_internal_purge_deleted_items.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[usp_sqlwatch_internal_purge_deleted_items] (
+	@purge_after_days tinyint = 30,
+	@row_batch_size int = 100
+	)
+as
+declare @sql varchar(max)
+set @sql = 'declare @rows_affected bigint'
+
+select @sql = @sql + '
+delete top (' + convert(varchar(10),@row_batch_size) + ') from ' + TABLE_SCHEMA + '.' + TABLE_NAME + '
+where ' + COLUMN_NAME + ' < dateadd(day,-' + convert(varchar(10),@purge_after_days) +',getutcdate())
+set @rows_affected = @@ROWCOUNT
+
+Print ''Purged '' + convert(varchar(10),@rows_affected) + '' rows from ' + TABLE_SCHEMA + '.' + TABLE_NAME + ' ''
+'
+ from INFORMATION_SCHEMA.COLUMNS
+WHERE COLUMN_NAME in ('deleted_when', 'date_deleted', 'last_seen')
+AND TABLE_NAME LIKE 'sqlwatch_meta%'
+
+set nocount on
+
+exec (@sql)
