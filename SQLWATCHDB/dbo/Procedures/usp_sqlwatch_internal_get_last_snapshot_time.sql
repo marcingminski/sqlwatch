e363022ad7188185edb9cb426aeae14739422403
Marcin Gminski <marcingminski@gmail.com>
2019-10-18 20:06:18 +0100

    new proc to get lastest snapshot from each table in central repository

---
+++ SQLWATCHDB/dbo/Procedures/usp_sqlwatch_internal_get_last_snapshot_time.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[usp_sqlwatch_internal_get_last_snapshot_time]
+	@sql_instance nvarchar(25),
+	@snapshot_type_id smallint
+AS
+	select [snapshot_time] = isnull(max([snapshot_time]),'1970-01-01') from [dbo].[sqlwatch_logger_snapshot_header]
+	where [sql_instance]= @sql_instance
+	and [snapshot_type_id] = @snapshot_type_id
