dc288887493a65c4733848aad9e051f2a7784bce
Marcin Gminski <marcingminski@gmail.com>
2019-10-18 20:04:06 +0100

    new procedure to enable table compression on all SQLWATCH tables

---
+++ SQLWATCHDB/dbo/Procedures/usp_sqlwatch_config_set_table_compression.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[usp_sqlwatch_config_set_table_compression]
+as
+
+declare @sql varchar(max)
+set @sql = ''
+select @sql = @sql + 'alter table ' + TABLE_SCHEMA + '.' + TABLE_NAME + ' rebuild partition = all with (data_compression = page);
+' 
+from INFORMATION_SCHEMA.TABLES
+WHERE TABLE_NAME LIKE '%sqlwatch_%'
+and TABLE_TYPE = 'BASE TABLE'
+
+print @sql 
+exec (@sql)
+
