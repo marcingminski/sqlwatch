2120a80fee1b17a820a81bbf87bb4c04caa3c882
Marcin Gminski <marcingminski@gmail.com>
2019-10-19 18:51:53 +0100

    Closes #89

--- SQLWATCHDB/dbo/Tables/sqlwatch_logger_xes_waits_stats.sql
+++ SQLWATCHDB/dbo/Tables/sqlwatch_logger_xes_waits_stats.sql
@@ -19,7 +19,7 @@
 	[activity_seqeuence_xfer] int null,
 	[event_name] [varchar](255) null,
 	[sql_instance] nvarchar(25) not null default @@SERVERNAME,
-	[sqlwatch_activity_id] uniqueidentifier default newsequentialid(),
+	[sqlwatch_activity_id] uniqueidentifier not null default newsequentialid(),
 	constraint fk_logger_xes_waits_snapshot_header foreign key ([snapshot_time],[snapshot_type_id],[sql_instance]) references [dbo].[sqlwatch_logger_snapshot_header]([snapshot_time],[snapshot_type_id],[sql_instance]) on delete cascade on update cascade,
 	constraint [pk_logger_xes_waits] primary key (
 		[snapshot_time],[snapshot_type_id],[sql_instance], [sqlwatch_activity_id]
