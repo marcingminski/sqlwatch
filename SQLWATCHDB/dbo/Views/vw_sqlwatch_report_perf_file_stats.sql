194a62e2f21410be35f4215c41adacfb30c69d1e
Marcin Gminski <marcingminski@gmail.com>
2019-10-21 00:03:54 +0100

    fixed missing column

--- SQLWATCHDB/dbo/Views/vw_sqlwatch_report_perf_file_stats.sql
+++ SQLWATCHDB/dbo/Views/vw_sqlwatch_report_perf_file_stats.sql
@@ -4,7 +4,7 @@ AS
  with cte_file_stats_sequence as (
 	select [sqlwatch_database_id], [sqlwatch_master_file_id], [num_of_reads], [num_of_bytes_read], [io_stall_read_ms], [num_of_writes], [num_of_bytes_written]
 		 , [io_stall_write_ms], [size_on_disk_bytes], [snapshot_time], [snapshot_type_id], [sql_instance] 
-		 , sequence=DENSE_RANK() over (partition by sql_instance, [sqlwatch_database_id], [sqlwatch_master_file_id] order by snapshot_time)
+		 , sequence=DENSE_RANK() over (partition by sql_instance, [sqlwatch_database_id], [sqlwatch_master_file_id] order by snapshot_time desc)
 	from [dbo].[sqlwatch_logger_perf_file_stats]
 	)
  select 
@@ -29,10 +29,12 @@ AS
                 ,[size_on_disk_mb]=fs2.[size_on_disk_bytes]/ 1024.0 / 1024.0
                 ,[size_on_disk_mb_delta]=(fs2.[size_on_disk_bytes]-fs1.[size_on_disk_bytes]) / 1024.0 / 1024.0
                 ,fs2.sql_instance
-                ,[is_latest] = case when fs2.[snapshot_time] = (
-					select max(t.snapshot_time) 
-					from [dbo].[sqlwatch_logger_snapshot_header] t
-					where t.snapshot_type_id = fs2.snapshot_type_id) then 1 else 0 end
+				,[is_latest] = case when fs2.sequence = 1 then 1 else 0 end
+     --           ,[is_latest] = case when fs2.[snapshot_time] = (
+					--select max(t.snapshot_time) 
+					--from [dbo].[sqlwatch_logger_snapshot_header] t
+					--where t.snapshot_type_id = fs2.snapshot_type_id
+					--and t.sql_instance = fs2.sql_instance) then 1 else 0 end
 				--,mdb.[database_name]
 				--,mf.file_physical_name
     from cte_file_stats_sequence  fs1
@@ -40,7 +42,7 @@ AS
 	inner join cte_file_stats_sequence  fs2 
 		on fs1.[sqlwatch_database_id] = fs2.[sqlwatch_database_id] 
 		and fs1.[sqlwatch_master_file_id] = fs2.[sqlwatch_master_file_id]
-		and fs1.sequence = fs2.sequence - 1
+		and fs1.sequence = fs2.sequence + 1
                 and fs2.snapshot_type_id = fs1.snapshot_type_id
                 and fs2.sql_instance = fs1.sql_instance
 
@@ -52,4 +54,4 @@ AS
 	--	on mdb.sql_instance = mf.sql_instance
 	--	and mdb.sqlwatch_database_id = mf.sqlwatch_database_id
                 
-	where mf.deleted_when is null
+	--where mf.deleted_when is null
