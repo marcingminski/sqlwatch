f15faaa8d741c50d5c7881b406978c5b97aa0269
Marcin Gminski <marcingminski@gmail.com>
2019-10-21 15:22:29 +0100

    pushed delta calculations to the logger to improve reporting performace

--- SQLWATCHDB/dbo/Tables/sqlwatch_logger_perf_file_stats.sql
+++ SQLWATCHDB/dbo/Tables/sqlwatch_logger_perf_file_stats.sql
@@ -13,6 +13,15 @@
 	[snapshot_time] datetime not null,
 	[snapshot_type_id] tinyint not null default 1 ,
 	[sql_instance] nvarchar(25) not null default @@SERVERNAME,
+
+	[num_of_reads_delta] real null,
+	[num_of_bytes_read_delta] real null,
+	[io_stall_read_ms_delta] real null,
+	[num_of_writes_delta] real null,
+	[num_of_bytes_written_delta] real null,
+	[io_stall_write_ms_delta] real null,
+	[size_on_disk_bytes_delta] real null,
+	[delta_seconds] int null,
 	constraint fk_sqlwatch_logger_perf_file_stats_master_file foreign key ([sql_instance], [sqlwatch_database_id], [sqlwatch_master_file_id]) 
 		references [dbo].[sqlwatch_meta_master_file] ([sql_instance], [sqlwatch_database_id], [sqlwatch_master_file_id]) on delete cascade,
 	constraint fk_sql_perf_mon_file_stats_snapshot_header foreign key ([snapshot_time],[snapshot_type_id], [sql_instance]) references [dbo].[sqlwatch_logger_snapshot_header]([snapshot_time],[snapshot_type_id], [sql_instance]) on delete cascade on update cascade,
--- SQLWATCHDB/dbo/Tables/sqlwatch_logger_perf_os_wait_stats.sql
+++ SQLWATCHDB/dbo/Tables/sqlwatch_logger_perf_os_wait_stats.sql
@@ -8,6 +8,12 @@
 	[snapshot_time] datetime not null,
 	[snapshot_type_id] tinyint not null default 1 ,
 	[sql_instance] nvarchar(25) not null default @@SERVERNAME,
+
+	[waiting_tasks_count_delta] real null,
+	[wait_time_ms_delta] real null,
+	[max_wait_time_ms_delta] real null,
+	[signal_wait_time_ms_delta] real null,
+	[delta_seconds] int null,
 	constraint fk_sql_perf_mon_wait_stats_snapshot_header foreign key ([snapshot_time],[snapshot_type_id],[sql_instance]) references [dbo].[sqlwatch_logger_snapshot_header]([snapshot_time],[snapshot_type_id],[sql_instance]) on delete cascade  on update cascade,
 	constraint [pk_sql_perf_mon_wait_stats] primary key (
 		[snapshot_time] asc, [snapshot_type_id] asc, [sql_instance] asc, [wait_type_id] asc
