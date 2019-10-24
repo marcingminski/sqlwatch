936275a2b9cc01a86beaa1bab6c4491b9d6d04c5
Marcin Gminski <marcingminski@gmail.com>
2019-10-24 14:22:06 +0100

    add meta sql_text to normalise queries, for future use.

---
+++ SQLWATCHDB/dbo/Tables/sqlwatch_meta_sql_text.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[sqlwatch_meta_sql_text]
+(
+	[sql_instance] varchar(32) NOT NULL,
+	[sql_text_id] bigint identity(1,1) not null,
+	[sql_text] nvarchar(max),
+	[sql_text_hash] as HASHBYTES('MD5', [sql_text]) persisted,
+	[date_added] datetime default getutcdate(),
+	[last_seen] datetime,
+	constraint pk_sqlwatch_meta_sql_text primary key clustered (
+		[sql_instance], [sql_text_id]
+		),
+	constraint uq_sqlwatch_meta_sql_text_hash unique (
+		[sql_text_hash]
+	),
+	constraint fk_sqlwatch_meta_sql_text foreign key ([sql_instance])
+		references [dbo].[sqlwatch_meta_server] ([servername]) on delete cascade
+)
