c2674eea41a762418cf141e6a19aff74c6fd7ee5
Marcin Gminski <marcingminski@gmail.com>
2019-10-24 11:40:07 +0100

    add config proc to enable index compression

---
+++ SQLWATCHDB/dbo/Procedures/usp_sqlwatch_config_set_index_compression.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[usp_sqlwatch_config_set_index_compression]
+as
+
+declare @sql varchar(max)
+set @sql = ''
+
+select @sql = @sql + 'alter index [' + idx.name +'] on ' + sh.name + '.' + tbl.name + ' rebuild partition = all with (data_compression = page);
+'
+from sys.indexes idx
+inner join sys.tables tbl
+	on tbl.object_id = idx.object_id
+inner join sys.schemas sh
+	on tbl.schema_id = sh.schema_id
+where tbl.name like 'sqlwatch%'
+and idx.name is not null
+
+print @sql
+exec (@sql)
