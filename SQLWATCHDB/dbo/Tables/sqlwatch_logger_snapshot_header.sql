cbb5cbf575f7065169f5ce4f094ab52fae00f82e
Marcin Gminski <marcingminski@gmail.com>
2019-10-24 12:26:44 +0100

    reduce data lenght

--- SQLWATCHDB/dbo/Tables/sqlwatch_logger_snapshot_header.sql
+++ SQLWATCHDB/dbo/Tables/sqlwatch_logger_snapshot_header.sql
@@ -3,8 +3,8 @@
 	[snapshot_time] datetime2(0),
 	[snapshot_type_id] tinyint not null default 1 foreign key references dbo.[sqlwatch_config_snapshot_type]([snapshot_type_id]),
 	[sql_instance] varchar(32) not null default @@SERVERNAME,
-	[report_time] as DATEADD(MINUTE, CEILING(DATEDIFF(SECOND, 0, CAST(CAST([snapshot_time] AS DATETIME) AS TIME)) / 60.0), DATEDIFF(DAY, 0, [snapshot_time])) persisted not null
-	 CONSTRAINT pk_snapshot PRIMARY KEY clustered (
+	[report_time] as convert(datetime2(0),DATEADD(MINUTE, CEILING(DATEDIFF(SECOND, 0, CAST(CAST([snapshot_time] AS DATETIME) AS TIME)) / 60.0), DATEDIFF(DAY, 0, [snapshot_time]))) persisted not null
+	 CONSTRAINT pk_snapshot PRIMARY KEY nonclustered (
 		[snapshot_time],[snapshot_type_id], [sql_instance]
 	)
 	--, constraint fk_snapshot_header_sql_instance foreign key (sql_instance) references dbo.sqlwatch_config_sql_instance (sql_instance) on delete cascade on update cascade
