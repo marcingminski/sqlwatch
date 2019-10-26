391069e75612764f3e24f43db251a768bdc6832e
Marcin Gminski <marcingminski@gmail.com>
2019-10-26 14:19:37 +0100

    moved wait stats categorisation from PBI

---
+++ SQLWATCHDB/dbo/Tables/sqlwatch_config_wait_stats.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[sqlwatch_config_wait_stats]
+(
+	[wait_type] nvarchar(60) not null,
+	[wait_category] nvarchar(60) not null,
+	[report_include] bit not null,
+	constraint pk_sqlwatch_config_wait_stats primary key (
+		[wait_type]
+		)
+)
