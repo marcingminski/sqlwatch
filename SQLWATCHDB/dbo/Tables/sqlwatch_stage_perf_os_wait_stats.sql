b85ec675b6a7529be98a264862be006d0ad48375
Marcin Gminski <marcingminski@gmail.com>
2019-10-26 14:22:24 +0100

    improved wait stats delta capture

---
+++ SQLWATCHDB/dbo/Tables/sqlwatch_stage_perf_os_wait_stats.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[sqlwatch_stage_perf_os_wait_stats]
+(
+	[wait_type] [nvarchar](60) NOT NULL,
+	[waiting_tasks_count] [bigint] NOT NULL,
+	[wait_time_ms] [bigint] NOT NULL,
+	[max_wait_time_ms] [bigint] NOT NULL,
+	[signal_wait_time_ms] [bigint] NOT NULL,
+	snapshot_time datetime2(0),
+	constraint pk_sqlwatch_stage_perf_os_wait_stats primary key clustered (
+		snapshot_time, wait_type
+		)
+)
