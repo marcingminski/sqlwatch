c999a1c4eeb7b33e1ae6b74598c8b70fd14368ff
Marcin Gminski <marcingminski@gmail.com>
2019-10-30 09:47:59 +0000

    a new function which is actually not required.

---
+++ SQLWATCHDB/dbo/Functions/ufn_sqlwatch_get_time_group.sql
@@ -0,0 +1,10 @@
+﻿CREATE FUNCTION [dbo].[ufn_sqlwatch_get_time_group]
+(
+	@report_time as datetime2(0),
+	@time_internval_minutes int
+)
+RETURNS smalldatetime
+AS
+BEGIN
+	RETURN convert(smalldatetime,dateadd(minute,(datediff(minute,0, @report_time)/ @time_internval_minutes) * @time_internval_minutes,0))
+END
