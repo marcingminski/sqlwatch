98aeee53dee66a6c9223591e1191aae89aafe38a
Marcin Gminski <marcingminski@gmail.com>
2019-10-30 09:45:40 +0000

    added pk

--- SQLWATCHDB/dbo/Tables/sqlwatch_config_sql_instance.sql
+++ SQLWATCHDB/dbo/Tables/sqlwatch_config_sql_instance.sql
@@ -8,7 +8,6 @@
 	[is_active] bit not null default 1,
 	[last_collection_time] datetime null,
 	[collection_status] varchar(50),
-	[utc_offset_minutes] int default DATEDIFF(mi, GETUTCDATE(), GETDATE()),
 	constraint pk_config_sql_instance primary key clustered (
 		[sql_instance]
 	)
