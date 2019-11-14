d1d0ae39e8a25c32158a1577902317f8af88425d
Marcin Gminski <marcingminski@gmail.com>
2019-11-14 22:14:36 +0000

    function to split tring into table

---
+++ SQLWATCHDB/dbo/Functions/ufn_sqlwatch_split_string.sql
@@ -0,0 +1,35 @@
+﻿CREATE FUNCTION [dbo].[ufn_sqlwatch_split_string]
+(
+	@input_string nvarchar(max),
+	@delimiter nvarchar(max)
+)
+RETURNS @output TABLE
+(
+	[value] nvarchar(max)
+)
+as
+begin
+
+      declare @string nvarchar(max)
+      declare @cnt Int 
+
+      if(@input_string is not null) 
+
+      begin
+            set @cnt = charindex(',',@input_string) 
+            while @cnt > 0 
+            begin 
+                  set @string = substring(@input_string,1,@cnt-1) 
+                  set @input_string = substring(@input_string,@cnt+1,len(@input_string)-@cnt) 
+
+                  insert into @output values (@string) 
+                  set @cnt = charindex(',',@input_string) 
+            end 
+
+
+            set @string = @input_string 
+
+            insert into @output values (@string) 
+      end
+      return
+end
