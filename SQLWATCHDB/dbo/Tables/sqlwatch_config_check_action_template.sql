a9e8c33b8791672fbf0c91b06241ab9f02b3c9c0
Marcin Gminski <marcingminski@gmail.com>
2019-11-16 21:16:05 +0000

    added update timestamp to detect modification by the end user

--- SQLWATCHDB/dbo/Tables/sqlwatch_config_check_action_template.sql
+++ SQLWATCHDB/dbo/Tables/sqlwatch_config_check_action_template.sql
@@ -8,7 +8,24 @@
 	[action_template_repeat_body] nvarchar(max) not null,
 	[action_template_recover_subject] nvarchar(max) not null,
 	[action_template_recover_body] nvarchar(max) not null,
+	[date_added] datetime default getdate() not null,
+	[date_updated] datetime null,
 	constraint pk_sqlwatch_config_action_template primary key clustered (
 		[action_template_id]
 	)
 )
+
+GO
+
+CREATE TRIGGER [dbo].[trg_sqlwatch_config_check_action_template_modify]
+    ON [dbo].[sqlwatch_config_check_action_template]
+    FOR UPDATE
+    AS
+    BEGIN
+        SET NoCount ON
+		update t
+			set [date_updated] = getdate()
+		from [dbo].[sqlwatch_config_check_action_template] t
+		inner join inserted u
+		on u.[action_template_id] = t.[action_template_id]
+    END
