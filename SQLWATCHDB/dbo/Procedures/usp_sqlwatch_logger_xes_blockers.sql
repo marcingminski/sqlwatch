3db290701b29f6f1f73d746426d2ae0f7d20d0b3
Marcin Gminski <marcingminski@gmail.com>
2019-11-17 13:11:29 +0000

    changes to the xes blockers collector to reduce number of rows collected and improve tranparency

--- SQLWATCHDB/dbo/Procedures/usp_sqlwatch_logger_xes_blockers.sql
+++ SQLWATCHDB/dbo/Procedures/usp_sqlwatch_logger_xes_blockers.sql
@@ -25,91 +25,150 @@ if [dbo].[ufn_sqlwatch_get_product_version]('major') >= 11
 		where xes.name = isnull((select name from sys.dm_xe_sessions where name = 'SQLWATCH_blockers'),'system_health')
 			and xet.target_name = 'ring_buffer'
 
-		insert into [dbo].[sqlwatch_logger_xes_blockers](
-			attach_activity_id, attach_activity_sequence,
-			blocking_start_time, blocking_end_time,
-			blocked_ecid, blocked_spid, blocked_sql,
-			[database_name], lock_mode, 
-			blocking_ecid, blocking_spid, blocking_sql, 
-			blocking_duration_ms, [blocking_client_app_name], [blocking_client_hostname],
-			report_xml,
-			snapshot_time, snapshot_type_id
-		)
-		select attach_activity_id, attach_activity_sequence,
-			blocking_start_time, blocking_end_time,
-			blocked_ecid, blocked_spid, blocked_sql,
-			[database name], lock_mode, 
-			blocking_ecid, blocking_spid, blocking_sql, 
-			blocking_duration_ms, [blocking_client app name], [blocking_client hostname],
-			report_xml,
-			@snapshot_time, @snapshot_type_id
-		from (
+
+		merge [dbo].[sqlwatch_logger_xes_blockers] as target
+		using 
+			(
 			select 
-				 [attach_activity_id]=substring(bps.[attach_activity_id],1,len(bps.[attach_activity_id])-charindex('-',reverse(bps.[attach_activity_id]))) 
-				,[attach_activity_sequence]=right(bps.[attach_activity_id],charindex('-',reverse(bps.[attach_activity_id]))-1)
-				,blocking_start_time = dateadd(ms,-bps.[blocking_duration_ms],bps.[report_end_time])
-				,blocking_end_time=bps.[report_end_time]
-				,bps.blocked_ecid
-				,bps.blocked_spid
-				,bps.blocked_sql
-				,bps.[database name]
-				,bps.[lock_mode]
-				,bps.blocking_ecid
-				,bps.blocking_spid
-				,bps.blocking_sql
-				,bps.[blocking_duration_ms]
-				,bps.[blocking_client app name]
-				,bps.[blocking_client hostname]
-				,bps.report_xml
-				/* SQL starts logging blocking chains as soon as they reach thresholds and NOT once the blocking chain has finished
-					this means we are going to get multiple entries per each chain until it has finished. we can go ahead and remove any previous
-					records of the same chain */
-				,session_blocking_sequence = row_number() over (partition by convert(datetime2(0),dateadd(ms,-bps.[blocking_duration_ms],bps.[report_end_time]))
-																							, bps.blocked_spid
-																							, bps.[database name]
-																							, bps.blocking_spid
-																							, bps.[blocking_client app name]
-																							, bps.[blocking_client hostname] 
-																					order by bps.[blocking_duration_ms] desc)
-			from #xes xet
-			--from sys.dm_xe_session_targets xet with (nolock)
-			--inner join sys.dm_xe_sessions xes with (nolock)
-			--	on xes.address = xet.event_session_address
-			--	/*  this will dynamically set session so the user has a choice to either use system_health session ot SQLWATCH_*. 
-			--		if SQLWATCH session is switched off we will use system_health otherwise use SQLWATCH_* */
-			--	and xes.name = isnull((select name from sys.dm_xe_sessions where name = 'SQLWATCH_blockers'),'system_health')
-			--	and xet.target_name = 'ring_buffer'
-			--from sys.fn_xe_file_target_read_file(@filename, null, null, null) xet
-			cross apply ( select cast(xet.target_data as xml) ) AS target_data ([xml])
-			cross apply target_data.[xml].nodes('/RingBufferTarget/event[@name="blocked_process_report"]') AS bp_nodes(bp_node)
-			cross apply bp_node.nodes('./data[@name="blocked_process"]/value/blocked-process-report') AS bp_report_xml_nodes(bp_report_xml_node)
-			cross apply
-				(
-					select 
-					 [report_xml] = cast(bp_report_xml_node.query('.') as xml)
-					,[report_end_time] = bp_node.value('(./@timestamp)[1]', 'datetime')
-					,[monitor_loop] = bp_report_xml_node.value('(//@monitorLoop)[1]', 'nvarchar(100)')
-					,[blocked_spid] = bp_report_xml_node.value('(./blocked-process/process/@spid)[1]', 'int')
-					,[blocked_ecid] = bp_report_xml_node.value('(./blocked-process/process/@ecid)[1]', 'int')
-					,[blocked_sql] = bp_report_xml_node.value('(./blocked-process/process/inputbuf)[1]', 'nvarchar(max)')
-					,[client app name] = bp_node.value('(./action[@name="client app name"]/value)[1]', 'nvarchar(128)')
-					,[database name] = nullif(bp_node.value('(./data[@name="database_name"]/value)[1]', 'nvarchar(128)'),'')
-					,[lock_mode]=bp_node.value('(./data[@name="lock_mode"]/text)[1]', 'varchar(50)')
-					,[blocking_spid] = bp_report_xml_node.value('(./blocking-process/process/@spid)[1]', 'int')
-					,[blocking_ecid] = bp_report_xml_node.value('(./blocking-process/process/@ecid)[1]', 'int')
-					,[blocking_sql] = bp_report_xml_node.value('(./blocking-process/process/inputbuf)[1]', 'varchar(max)')
-					,[blocking_duration_ms] = bp_node.value('(./data[@name="duration"]/value)[1]', 'bigint')/1000
-					,[blocking_username]=bp_report_xml_node.value('(./action[@name="username"]/value)[1]', 'nvarchar(128)')
-					,[blocking_client app name] = bp_report_xml_node.value('(./blocking-process/process/@clientapp)[1]', 'nvarchar(128)')
-					,[blocking_client hostname] = bp_report_xml_node.value('(./blocking-process/process/@hostname)[1]', 'nvarchar(128)')
-					,[attach_activity_id] = bp_node.value('(./action[@name="attach_activity_id"]/value )[1]', 'varchar(255)')
-				) AS bps
-			left join [dbo].[sqlwatch_logger_xes_blockers] t
-				on t.attach_activity_id = substring(bps.[attach_activity_id],1,len(bps.[attach_activity_id])-charindex('-',reverse(bps.[attach_activity_id]))) 
-				and t.attach_activity_sequence = right(bps.[attach_activity_id],charindex('-',reverse(bps.[attach_activity_id]))-1)
-			where t.attach_activity_id is null
-		) t
-		where session_blocking_sequence = 1
+				  attach_activity_id
+				, attach_activity_sequence
+				, blocking_start_time
+				, blocking_end_time
+				, blocked_ecid
+				, blocked_spid
+				, blocked_sql
+				, [database name]
+				, lock_mode
+				, blocking_ecid
+				, blocking_spid
+				, blocking_sql
+				, blocking_duration_ms
+				, [blocking_client app name]
+				, [blocking_client hostname]
+				, report_xml
+				, snapshot_time = @snapshot_time
+				, snapshot_type_id = @snapshot_type_id
+				, [sql_instance] = @@SERVERNAME
+			from (
+				select 
+					 [attach_activity_id]=substring(bps.[attach_activity_id],1,len(bps.[attach_activity_id])-charindex('-',reverse(bps.[attach_activity_id]))) 
+					,[attach_activity_sequence]=right(bps.[attach_activity_id],charindex('-',reverse(bps.[attach_activity_id]))-1)
+					,blocking_start_time = dateadd(ms,-bps.[blocking_duration_ms],bps.[report_end_time])
+					,blocking_end_time=bps.[report_end_time]
+					,bps.blocked_ecid
+					,bps.blocked_spid
+					,bps.blocked_sql
+					,bps.[database name]
+					,bps.[lock_mode]
+					,bps.blocking_ecid
+					,bps.blocking_spid
+					,bps.blocking_sql
+					,bps.[blocking_duration_ms]
+					,bps.[blocking_client app name]
+					,bps.[blocking_client hostname]
+					,bps.report_xml
+					/* SQL starts logging blocking chains as soon as they reach thresholds and NOT once the blocking chain has finished
+						this means we are going to get multiple entries per each chain until it has finished. we can go ahead and remove any previous
+						records of the same chain */
+					,session_blocking_sequence = row_number() over (partition by convert(datetime2(0),dateadd(ms,-bps.[blocking_duration_ms],bps.[report_end_time]))
+																								, bps.blocked_spid
+																								, bps.[database name]
+																								, bps.blocking_spid
+																								, bps.[blocking_client app name]
+																								, bps.[blocking_client hostname] 
+																						order by bps.[blocking_duration_ms] desc)
+				from #xes xet
+				cross apply ( select cast(xet.target_data as xml) ) AS target_data ([xml])
+				cross apply target_data.[xml].nodes('/RingBufferTarget/event[@name="blocked_process_report"]') AS bp_nodes(bp_node)
+				cross apply bp_node.nodes('./data[@name="blocked_process"]/value/blocked-process-report') AS bp_report_xml_nodes(bp_report_xml_node)
+				cross apply
+					(
+						select 
+						 [report_xml] = cast(bp_report_xml_node.query('.') as xml)
+						,[report_end_time] = bp_node.value('(./@timestamp)[1]', 'datetime')
+						,[monitor_loop] = bp_report_xml_node.value('(//@monitorLoop)[1]', 'nvarchar(100)')
+						,[blocked_spid] = bp_report_xml_node.value('(./blocked-process/process/@spid)[1]', 'int')
+						,[blocked_ecid] = bp_report_xml_node.value('(./blocked-process/process/@ecid)[1]', 'int')
+						,[blocked_sql] = bp_report_xml_node.value('(./blocked-process/process/inputbuf)[1]', 'nvarchar(max)')
+						,[client app name] = bp_node.value('(./action[@name="client app name"]/value)[1]', 'nvarchar(128)')
+						,[database name] = nullif(bp_node.value('(./data[@name="database_name"]/value)[1]', 'nvarchar(128)'),'')
+						,[lock_mode]=bp_node.value('(./data[@name="lock_mode"]/text)[1]', 'varchar(50)')
+						,[blocking_spid] = bp_report_xml_node.value('(./blocking-process/process/@spid)[1]', 'int')
+						,[blocking_ecid] = bp_report_xml_node.value('(./blocking-process/process/@ecid)[1]', 'int')
+						,[blocking_sql] = bp_report_xml_node.value('(./blocking-process/process/inputbuf)[1]', 'varchar(max)')
+						,[blocking_duration_ms] = bp_node.value('(./data[@name="duration"]/value)[1]', 'bigint')/1000
+						,[blocking_username]=bp_report_xml_node.value('(./action[@name="username"]/value)[1]', 'nvarchar(128)')
+						,[blocking_client app name] = bp_report_xml_node.value('(./blocking-process/process/@clientapp)[1]', 'nvarchar(128)')
+						,[blocking_client hostname] = bp_report_xml_node.value('(./blocking-process/process/@hostname)[1]', 'nvarchar(128)')
+						,[attach_activity_id] = bp_node.value('(./action[@name="attach_activity_id"]/value )[1]', 'varchar(255)')
+					) AS bps
+				left join [dbo].[sqlwatch_logger_xes_blockers] t
+					on t.attach_activity_id = substring(bps.[attach_activity_id],1,len(bps.[attach_activity_id])-charindex('-',reverse(bps.[attach_activity_id]))) 
+					and t.attach_activity_sequence = right(bps.[attach_activity_id],charindex('-',reverse(bps.[attach_activity_id]))-1)
+				where t.attach_activity_id is null
+			) t
+			where session_blocking_sequence = 1
+		) as source
+	    /* if a blocking chain already exists, we are going to update it with the most recent data as per the comment above */
+		on      source.[sql_instance] = target.[sql_instance]
+			and source.[blocked_ecid] = target.[blocked_ecid]
+			and source.[blocked_spid] = target.[blocked_spid]
+			and source.[blocked_sql] = target.[blocked_sql]
+			and source.[database name] = target.[database_name]
+			and source.[lock_mode] = target.[lock_mode]
+			and source.[blocking_ecid] = target.[blocking_ecid]
+			and source.[blocking_spid] = target.[blocking_spid]
+			and source.[blocking_sql] = target.[blocking_sql]
+			and source.[blocking_client app name] = target.[blocking_client_app_name]
+			and source.[blocking_client hostname] = target.[blocking_client_hostname]
+			and convert(datetime2(0),source.blocking_start_time) = convert(datetime2(0),target.blocking_start_time) --it may vary by few miliseconds
+
+		when not matched then
+			insert (  attach_activity_id
+					, attach_activity_sequence
+					, blocking_start_time
+					, blocking_end_time
+					, blocked_ecid
+					, blocked_spid
+					, blocked_sql
+					, [database_name]
+					, lock_mode
+					, blocking_ecid
+					, blocking_spid
+					, blocking_sql
+					, blocking_duration_ms
+					, [blocking_client_app_name]
+					, [blocking_client_hostname]
+					, report_xml
+					, snapshot_time
+					, snapshot_type_id)
+			values (  source.attach_activity_id
+					, source.attach_activity_sequence
+					, source.blocking_start_time
+					, source.blocking_end_time
+					, source.blocked_ecid
+					, source.blocked_spid
+					, source.blocked_sql
+					, source.[database name]
+					, source.lock_mode
+					, source.blocking_ecid
+					, source.blocking_spid
+					, source.blocking_sql
+					, source.blocking_duration_ms
+					, source.[blocking_client app name]
+					, source.[blocking_client hostname]
+					, source.report_xml
+					, source.snapshot_time
+					, source.snapshot_type_id)
+
+		when matched and source.blocking_duration_ms > target.blocking_duration_ms then 
+			update set
+					  blocking_start_time = source.blocking_start_time --it may vary by few miliseconds
+					, blocking_end_time = source.blocking_end_time
+					, blocking_duration_ms = source.blocking_duration_ms
+					, report_xml = source.report_xml
+					, snapshot_time = @snapshot_time
+
 		option (maxdop 1);
 	end
 else
