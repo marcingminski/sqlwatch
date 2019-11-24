8f4e844dcb72c2e2302ca2ae5ff838c8274f7e46
Marcin Gminski <marcingminski@gmail.com>
2019-11-24 21:50:13 +0000

    capture additional database settings for alerting

--- SQLWATCHDB/dbo/Procedures/usp_sqlwatch_internal_add_database.sql
+++ SQLWATCHDB/dbo/Procedures/usp_sqlwatch_internal_add_database.sql
@@ -5,19 +5,23 @@ as
 	declare @databases table (
 		[database_name] sysname not null,
 		[database_create_date] datetime not null default '1970-01-01',
-		[sql_instance] varchar(32) not null
+		[sql_instance] varchar(32) not null,
+		[is_auto_close_on] bit,
+		[is_auto_shrink_on] bit,
+		[is_auto_update_stats_on] bit,
 		primary key clustered (
 		 [database_name],[database_create_date], [sql_instance]
 	 )
 )
 
 	insert into @databases
-	select [name], [create_date], [sql_instance] = @@SERVERNAME
+	select [name], [create_date], [sql_instance]
+		, [is_auto_close_on], [is_auto_shrink_on], [is_auto_update_stats_on]
 	from dbo.vw_sqlwatch_sys_databases
 	union all
 	/* mssqlsystemresource database appears in the performance counters
 	so we need it as a dimensions to be able to filter in the report */
-	select 'mssqlsystemresource', '1970-01-01', @@SERVERNAME
+	select 'mssqlsystemresource', '1970-01-01', @@SERVERNAME, null, null, null
 	
 
 	/*	using database_create_data to distinguish databases that have been dropped and re-created 
@@ -31,12 +35,17 @@ as
 			and source.[sql_instance] = target.[sql_instance]
 		)
 	/* dropped databases are going to be updated to current = 0 */
-	--when not matched by source and target.sql_instance = @@SERVERNAME then
-	--	update set deleted_when = GETUTCDATE()
+	when matched then
+		update set [date_last_seen] = GETUTCDATE(),
+			[is_auto_close_on] = source.[is_auto_close_on],
+			[is_auto_shrink_on] = source.[is_auto_shrink_on],
+			[is_auto_update_stats_on] = source.[is_auto_update_stats_on]
+			
 	/* new databases are going to be inserted */
 	when not matched by target then
-		insert ([database_name], [database_create_date], [sql_instance])
-		values (source.[database_name], source.[database_create_date], source.[sql_instance]);
+		insert ([database_name], [database_create_date], [sql_instance], [is_auto_close_on], [is_auto_shrink_on], [is_auto_update_stats_on])
+		values (source.[database_name], source.[database_create_date], source.[sql_instance]
+			, source.[is_auto_close_on], source.[is_auto_shrink_on], source.[is_auto_update_stats_on]);
 
 	/*	the above only accounts for databases that have been removed and re-added
 		if you rename database it will be treated as if it was removed and new
