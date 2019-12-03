0a3174ebad27004c18fcaa1f3738ce43d6618906
Marcin Gminski <marcingminski@gmail.com>
2019-12-03 15:14:43 +0000

    remove not needed columns and RI to be able to load into central repo

--- SQLWATCHDB/dbo/Tables/sqlwatch_logger_report_action.sql
+++ SQLWATCHDB/dbo/Tables/sqlwatch_logger_report_action.sql
@@ -5,12 +5,13 @@
 	[snapshot_type_id] tinyint not null constraint df_sqlwatch_logger_report_action_type default (18),
 	[report_id] smallint not null,
 	[action_id] smallint not null,
-	[action_type] varchar(50),
-	[action_subject] nvarchar(max),
-	[action_body] nvarchar(max),
 	constraint pk_sqlwatch_logger_report_action_action primary key clustered (
-		[snapshot_time], [sql_instance], [snapshot_type_id], [action_id]
+		[snapshot_time], [sql_instance], [snapshot_type_id], [report_id], [action_id]
 	),
-	constraint fk_sqlwatch_logger_report_action_action foreign key ([action_id])	
-		references [dbo].[sqlwatch_config_action] ([action_id]) on delete cascade
+	/* replaced by trigger on config we can "detach it" and send to central repo */
+	--constraint fk_sqlwatch_logger_report_action_action foreign key ([action_id])	
+	--	references [dbo].[sqlwatch_config_action] ([action_id])
+
+	--constraint fk_sqlwatch_logger_report_action_report foreign key ([report_id])
+	--	references [dbo].[sqlwatch_config_report] ([report_id]) on delete cascade
 )
