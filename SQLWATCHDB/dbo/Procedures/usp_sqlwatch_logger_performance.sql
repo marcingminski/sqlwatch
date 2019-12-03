f24335667d37d92f46064f777ce51e2827f5b243
Marcin Gminski <marcingminski@gmail.com>
2019-12-03 15:12:26 +0000

    Comments

--- SQLWATCHDB/dbo/Procedures/usp_sqlwatch_logger_performance.sql
+++ SQLWATCHDB/dbo/Procedures/usp_sqlwatch_logger_performance.sql
@@ -88,22 +88,39 @@ declare @sql nvarchar(4000)
 		insert into dbo.[sqlwatch_logger_perf_os_performance_counters] ([performance_counter_id],[instance_name], [cntr_value], [base_cntr_value],
 			[snapshot_time], [snapshot_type_id], [sql_instance], [cntr_value_calculated])
 		select
-			 --[object_name] = rtrim(pc.[object_name])
 			 mc.[performance_counter_id]
 			,instance_name = rtrim(pc.instance_name)
-			--,counter_name = rtrim(pc.counter_name)
 			,pc.cntr_value
 			,base_cntr_value=bc.cntr_value
-			--,pc.cntr_type
 			,snapshot_time=@date_snapshot_current
 			, @snapshot_type_id
 			, @@SERVERNAME
 			,[cntr_value_calculated] = convert(real,(
 				case 
+					--https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.performancecountertype?view=netframework-4.8
+					--https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.performancedata.countertype?view=netframework-4.8
 					when mc.object_name = 'Batch Resp Statistics' then case when pc.cntr_value > prev.cntr_value then cast((pc.cntr_value - prev.cntr_value) as real) else 0 end -- delta absolute
-					when mc.cntr_type = 65792 then isnull(pc.cntr_value,0) -- point-in-time
+					
+					/*	65792
+						An instantaneous counter that shows the most recently observed value. Used, for example, to maintain a simple count of a very large number of items or operations. 
+						It is the same as NumberOfItems32 except that it uses larger fields to accommodate larger values.	*/
+					when mc.cntr_type = 65792 then isnull(pc.cntr_value,0) 	
+					
+					/*	272696576
+						A difference counter that shows the average number of operations completed during each second of the sample interval. Counters of this type measure time in ticks of the system clock. 
+						This counter type is the same as the RateOfCountsPerSecond32 type, but it uses larger fields to accommodate larger values to track a high-volume number of items or operations per second, 
+						such as a byte-transmission rate. Counters of this type include System\ File Read Bytes/sec.	*/
 					when mc.cntr_type = 272696576 then case when (pc.cntr_value > prev.cntr_value) then (pc.cntr_value - prev.cntr_value) / cast(datediff(second,prev.snapshot_time,@date_snapshot_current) as real) else 0 end -- delta rate
+					
+					/*	537003264	
+						This counter type shows the ratio of a subset to its set as a percentage. For example, it compares the number of bytes in use on a disk to the total number of bytes on the disk. 
+						Counters of this type display the current percentage only, not an average over time. It is the same as the RawFraction32 counter type, except that it uses larger fields to accommodate larger values.	*/
 					when mc.cntr_type = 537003264 then isnull(cast(100.0 as real) * pc.cntr_value / nullif(bc.cntr_value, 0),0) -- ratio
+
+					/*	1073874176		
+						An average counter that shows how many items are processed, on average, during an operation. Counters of this type display a ratio of the items processed to the number of operations completed. 
+						The ratio is calculated by comparing the number of items processed during the last interval to the number of operations completed during the last interval. 
+						Counters of this type include PhysicalDisk\ Avg. Disk Bytes/Transfer.	*/
 					when mc.cntr_type = 1073874176 then isnull(case when pc.cntr_value > prev.cntr_value then isnull((pc.cntr_value - prev.cntr_value) / nullif(bc.cntr_value - prev.base_cntr_value, 0) / cast(datediff(second,prev.snapshot_time,@date_snapshot_current) as real), 0) else 0 end,0) -- delta ratio
 				end))
 		from (
