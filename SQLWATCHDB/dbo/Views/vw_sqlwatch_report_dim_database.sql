25ec5f85ae0bb935509a8cf8e33717ba394b7f00
Marcin Gminski <marcingminski@gmail.com>
2019-12-10 14:11:17 +0000

    add is_current flag

--- SQLWATCHDB/dbo/Views/vw_sqlwatch_report_dim_database.sql
+++ SQLWATCHDB/dbo/Views/vw_sqlwatch_report_dim_database.sql
@@ -13,6 +13,7 @@ as
 		, index_size_bytes_current = lg.index_size_bytes
 		, [unused_bytes_current] = lg.unused_bytes
 		, unallocated_space_bytes_current = lg.unallocated_space_bytes
+		, is_current = case when ROW_NUMBER() over (partition by [database_name] order by [database_create_date] desc) = 1 then 1 else 0 end
 
 		from dbo.sqlwatch_meta_database d
 
@@ -53,13 +54,15 @@ as
 				and g2.sqlwatch_database_id = vc.sqlwatch_database_id
 				) lg
 	) , cte_database_growth as (
-	select [database_name], [database_create_date], [sql_instance], [sqlwatch_database_id], [date_last_seen], [database_size_bytes_current], [database_bytes_growth], [total_growth_days] , [log_size_total_bytes_current], [log_size_bytes_growth]
+	select [database_name], [database_create_date], [sql_instance], [sqlwatch_database_id], [date_last_seen]
+		, [database_size_bytes_current], [database_bytes_growth], [total_growth_days] , [log_size_total_bytes_current], [log_size_bytes_growth]
 		, database_growth_bytes_per_day = case when [total_growth_days] > 0 and [database_bytes_growth] > 0 then [database_bytes_growth] / total_growth_days else 0 end
 		, log_growth_bytes_per_day = case when [total_growth_days] > 0 and [log_size_bytes_growth] > 0 then [log_size_bytes_growth] / total_growth_days else 0 end
 		, data_bytes_current, index_size_bytes_current, [unused_bytes_current], unallocated_space_bytes_current
 	from cte_database
 	)
-	select [database_name], [database_create_date], [sql_instance], [sqlwatch_database_id], [date_last_seen], [database_size_bytes_current], [database_bytes_growth]
+	select [database_name], [database_create_date], [sql_instance], [sqlwatch_database_id], [date_last_seen]
+		, [database_size_bytes_current], [database_bytes_growth]
 		, [total_growth_days], database_growth_bytes_per_day, log_growth_bytes_per_day, [log_size_total_bytes_current]
 		, data_bytes_current, index_size_bytes_current, [unused_bytes_current], unallocated_space_bytes_current
 	, [database_size_bytes_current_formatted] = case 
