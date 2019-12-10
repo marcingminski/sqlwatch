6eeaab8f27b1dcf2e9f4727bd281ef66019e222e
Marcin Gminski <marcingminski@gmail.com>
2019-12-10 13:00:04 +0000

    Performance improvement, Add retention for trends

--- SQLWATCHDB/dbo/Procedures/usp_sqlwatch_internal_retention.sql
+++ SQLWATCHDB/dbo/Procedures/usp_sqlwatch_internal_retention.sql
@@ -28,49 +28,63 @@ as
  Change Log:
 	1.0		2019-08		- Marcin Gminski, Initial version
 	1.1		2019-11-29	- Marcin Gminski, Ability to only leave most recent snapshot with -1 retention
-	1.2		2012-12-07	- Marcin Gminski, Added retention of the action queue
+	1.2		2019-12-07	- Marcin Gminski, Added retention of the action queue
+	1.3		2019-12-09	- Marcin Gminski, build deletion keys out of the loop to improve loop performance and reduce contention
 -------------------------------------------------------------------------------------------------------------------
 */
 set nocount on;
+
+declare @snapshot_type_id tinyint
+
+declare @cutoff_dates as table (
+	snapshot_time datetime2(0),
+	sql_instance varchar(32),
+	snapshot_type_id tinyint,
+	primary key ([sql_instance], [snapshot_type_id])
+)
+	
+/*	To account for central repository, we need a list of all possible snapshot types cross joined with servers list
+	and calculate retention times from the type. This cannot be done for retention -1 as for that scenarios, 
+	we need to know the latest current snapshot.	*/
+insert into @cutoff_dates
+	select snapshot_time = case when st.snapshot_retention_days >0 then dateadd(day,-st.snapshot_retention_days,GETUTCDATE()) else null end
+		, si.sql_instance
+		, st.snapshot_type_id
+	from [dbo].[sqlwatch_config_snapshot_type] st
+	cross join [dbo].[sqlwatch_config_sql_instance] si
+
+/*	Once we have a list of snapshots and dates, 
+	we can get max snapshot for the rest - to avoid excesive scanning
+	and try force a seek, we are limiting this to only those have not got date yet i.e. snapshot types = -1	*/
+update c
+	set snapshot_time = t.snapshot_time
+from @cutoff_dates c
+inner join (
+	select snapshot_time=max(sh.snapshot_time), sh.sql_instance, sh.snapshot_type_id
+	from dbo.sqlwatch_logger_snapshot_header sh
+	inner join @cutoff_dates cd
+		on cd.sql_instance = sh.sql_instance collate database_default
+		and cd.snapshot_type_id = sh.snapshot_type_id
+	where cd.snapshot_time is null
+	group by sh.sql_instance, sh.snapshot_type_id
+	) t
+on t.sql_instance = c.sql_instance collate database_default
+and t.snapshot_type_id = c.snapshot_type_id
+
+
 declare @row_count int = 1
 while @row_count > 0
 	begin
 		begin tran
-			delete 
-			from dbo.[sqlwatch_logger_snapshot_header]
-			where exists (
-					select top (@batch_size) sh.[snapshot_time], sh.snapshot_type_id, st.snapshot_retention_days
-					from dbo.[sqlwatch_logger_snapshot_header] sh
-					inner join [dbo].[sqlwatch_config_snapshot_type] st
-						on sh.[snapshot_type_id] = st.[snapshot_type_id]
+			delete top (@batch_size) h
+			from dbo.[sqlwatch_logger_snapshot_header] h (readpast)
+			inner join @cutoff_dates c 
+				on h.snapshot_time < c.snapshot_time
+				and h.sql_instance = c.sql_instance
+				and h.snapshot_type_id = c.snapshot_type_id
 
-					/*	2019-11-29, in version 2.2 I have introduced new retention type of -1 which means:
-					    "delete all but the most recent snapshot". This is handy for keeping only the most recent
-						snapshot of index stats or histograms which otherwise can get quite large. In order to 
-						achieve that, we had to add a join and find the most recent snapshot for each type. 
-						
-						This change has increased execution time from ~40ms to ~150ms on the same table sizes.
-						However, to counter act that, I have moved TOP into the select rather than delete
-						which brings it down from ~150ms to ~4ms so much quicker than the original	*/
-					left join (
-						 select snapshot_time=max(snapshot_time), sh2.snapshot_type_id, sh2.sql_instance
-						 from dbo.[sqlwatch_logger_snapshot_header] sh2
-						 inner join [dbo].[sqlwatch_config_snapshot_type] st2
-							on sh2.[snapshot_type_id] = st2.[snapshot_type_id]
-						 where st2.snapshot_retention_days = -1
-						 group by sh2.snapshot_type_id, sh2.sql_instance
-						) m
-						on m.snapshot_type_id = st.snapshot_type_id
-						and m.snapshot_time = sh.report_time
-						and m.sql_instance = sh.sql_instance
-
-					where sh.snapshot_time < case 
-						when st.snapshot_retention_days = -1 then m.snapshot_time 
-						else dateadd(day,-st.snapshot_retention_days,getutcdate()) end
-					and dbo.[sqlwatch_logger_snapshot_header].[snapshot_time] = sh.[snapshot_time]
-					and dbo.[sqlwatch_logger_snapshot_header].snapshot_type_id = sh.snapshot_type_id
-			)
 			set @row_count = @@ROWCOUNT
+			print 'Deleted: ' + convert(varchar(max),@row_count) + case when @row_count = 1 then ' row' else ' rows' end
 		commit tran
 	end
 
@@ -78,4 +92,21 @@ while @row_count > 0
 	delete 
 	from [dbo].[sqlwatch_meta_action_queue] 
 	where [time_queued] < case when exec_status <> 'FAILED' then dateadd(day,-1,sysdatetime()) else dateadd(day,-7,sysdatetime()) end
+	Print 'Deleted ' + convert(varchar(max),@@ROWCOUNT) + ' record' + case when @@ROWCOUNT > 1 then 's' else '' end + ' from [dbo].[sqlwatch_meta_action_queue]'
+
+	/*	Trend tables retention.
+		These are detached from the header so we can keep more history and in a slightly different format to utilise less storage.
+		We are going to have remove data from these tables manually	*/
+
+	set @snapshot_type_id = 1 --Performance Counters
+	delete from [dbo].[sqlwatch_trend_perf_os_performance_counters]
+	where sql_instance = @@SERVERNAME
+	and snapshot_time < (
+		select dateadd(day,-[snapshot_retention_days_trend],getutcdate())
+		from [dbo].[sqlwatch_config_snapshot_type]
+		where snapshot_type_id = @snapshot_type_id
+		and [snapshot_retention_days_trend] is not null
+		)
+	Print 'Deleted ' + convert(varchar(max),@@ROWCOUNT) + ' record' + case when @@ROWCOUNT > 1 then 's' else '' end + ' from [dbo].[sqlwatch_trend_perf_os_performance_counters]'
+
 go
