f5c6b8d4d067b67135c8c5f747261175e00d0eb6
Marcin Gminski <marcingminski@gmail.com>
2019-12-16 13:44:43 +0000

    Add denormalied columns for easier local debugging

---
+++ SQLWATCHDB/dbo/Functions/ufn_sqlwatch_get_file_type_desc.sql
@@ -0,0 +1,26 @@
+﻿CREATE FUNCTION [dbo].[ufn_sqlwatch_get_file_type_desc] 
+(
+	@file_type tinyint
+) 
+RETURNS varchar(max)
+with schemabinding
+AS
+BEGIN
+/* https://docs.microsoft.com/en-us/sql/relational-databases/system-catalog-views/sys-master-files-transact-sql
+		
+File type:
+0 = Rows.
+1 = Log
+2 = FILESTREAM
+3 = Identified for informational purposes only. Not supported. Future compatibility is not guaranteed.
+4 = Full-text (Full-text catalogs earlier than SQL Server 2008; full-text catalogs that are upgraded to or created in SQL Server 2008 or higher will report a file type 0.)
+
+*/
+		return  case @file_type
+			when 0 then 'Rows'
+			when 1 then 'Log'
+			when 2 then 'FILESTREAM'
+			when 3 then '3'
+			when 4 then 'Full-text'
+		else 'UNKNOWN' end
+END
--- SQLWATCHDB/dbo/Views/vw_sqlwatch_report_fact_disk_utilisation_volume.sql
+++ SQLWATCHDB/dbo/Views/vw_sqlwatch_report_fact_disk_utilisation_volume.sql
@@ -2,6 +2,7 @@
 as
 
 SELECT d.[sqlwatch_volume_id]
+	  ,v.volume_name
       ,[volume_free_space_bytes]
       ,[volume_total_space_bytes]
       ,h.report_time
@@ -9,7 +10,21 @@ SELECT d.[sqlwatch_volume_id]
  --for backward compatibility with existing pbi, this column will become report_time as we could be aggregating many snapshots in a report_period
 , d.snapshot_time
   FROM [dbo].[sqlwatch_logger_disk_utilisation_volume] d
+
   	inner join dbo.sqlwatch_logger_snapshot_header h
 		on  h.snapshot_time = d.[snapshot_time]
 		and h.snapshot_type_id = d.snapshot_type_id
 		and h.sql_instance = d.sql_instance
+
+	/*  using outer apply instead of inner join is SOO MUCH slower...
+		BUT it only applies to the columns we select.
+		If we do not select any columns from the outer apply, it does not get applied whereas joins
+		always do whether we select columns or not. 99% of the time these views will feed PowerBI wher only IDs are required
+		and small subset of columns queried. that 1% will be DBAs querying views directly in SSMS (TOP (1000)) in which case, 
+		having actual names instead alongisde IDs will make their life easier with small increase in performane penalty */
+	outer apply (
+		select [volume_name]
+		from [dbo].[sqlwatch_meta_os_volume] mov
+		where mov.sql_instance = d.sql_instance
+		and mov.sqlwatch_volume_id = d.sqlwatch_volume_id
+	) v
