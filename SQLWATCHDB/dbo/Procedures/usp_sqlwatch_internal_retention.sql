1ef1eb8e01ba94eab21bbca503ffb3d9962e589b
Marcin Gminski <marcingminski@gmail.com>
2020-01-12 16:18:06 +0000

    use report time instead of snapshot time as we are gong to lose header for trends and thus lose utc timing. we have to keep in the table itself.

--- SQLWATCHDB/dbo/Procedures/usp_sqlwatch_internal_retention.sql
+++ SQLWATCHDB/dbo/Procedures/usp_sqlwatch_internal_retention.sql
@@ -124,7 +124,7 @@ while @row_count > 0
 	set @snapshot_type_id = 1 --Performance Counters
 	delete from [dbo].[sqlwatch_trend_perf_os_performance_counters]
 	where sql_instance = @@SERVERNAME
-	and snapshot_time < (
+	and [report_time] < (
 		select dateadd(day,-[snapshot_retention_days_trend],getutcdate())
 		from [dbo].[sqlwatch_config_snapshot_type]
 		where snapshot_type_id = @snapshot_type_id
