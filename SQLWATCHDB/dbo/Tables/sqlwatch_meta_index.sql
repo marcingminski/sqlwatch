b7d1492afd8bee5f25124b93e6b68edc9ab4979d
Marcin Gminski <marcingminski@gmail.com>
2020-06-26 12:21:18 +0100

    3.0.7482.22204 - fix #178. It is possible to have index and table with the same name and table will come as HEAP in the indexes table.

--- SQLWATCHDB/dbo/Tables/sqlwatch_meta_index.sql
+++ SQLWATCHDB/dbo/Tables/sqlwatch_meta_index.sql
@@ -15,8 +15,8 @@
 		[sql_instance],[sqlwatch_database_id], [sqlwatch_table_id], [sqlwatch_index_id]
 		),
 	constraint fk_sqlwatch_meta_index foreign key ([sql_instance],[sqlwatch_database_id],[sqlwatch_table_id]) 
-		references [dbo].[sqlwatch_meta_table] ([sql_instance],[sqlwatch_database_id],[sqlwatch_table_id]) on delete cascade,
-	constraint uq_sqlwatch_meta_index unique (
-		[sql_instance], [sqlwatch_database_id], [sqlwatch_table_id], [index_name]
-		)
+		references [dbo].[sqlwatch_meta_table] ([sql_instance],[sqlwatch_database_id],[sqlwatch_table_id]) on delete cascade
+	--constraint uq_sqlwatch_meta_index unique (
+	--	[sql_instance], [sqlwatch_database_id], [sqlwatch_table_id], [index_name]
+	--	)
 )
