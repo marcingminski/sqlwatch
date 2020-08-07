9adc13433315828367f79dc0700567706729f0fc
Marcin Gminski <8288333+marcingminski@users.noreply.github.com>
2020-08-07 18:15:51 +0100

    Changed folder structure to better utilise Solutions and Projects after the solution has grown and now spans across multiple projects.

---
+++ SqlWatch.Monitor/Project.SqlWatch.Database/Script.PostDeployment1.sql
@@ -0,0 +1,120 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+--------------------------------------------------------------------------------------
+-- THIS MUST BE THE FIRST STEP:
+-- Load local server to satisfy RI
+--------------------------------------------------------------------------------------
+:r .\Scripts\Post-Deployment\Reference-Data\Script.PostDeployment-LoadMeta-Server.sql
+
+--------------------------------------------------------------------------------------
+-- Add Databases to the reference tables
+--------------------------------------------------------------------------------------
+exec [dbo].[usp_sqlwatch_internal_add_database]
+
+--------------------------------------------------------------------------------------
+-- Load Default Performance Counters
+--------------------------------------------------------------------------------------
+:r .\Scripts\Post-Deployment\Reference-Data\Script.PostDeployment-LoadConfig-PerformanceCounters.sql
+
+--------------------------------------------------------------------------------------
+-- Load Default Snapshot Types
+--------------------------------------------------------------------------------------
+:r .\Scripts\Post-Deployment\Reference-Data\Script.PostDeployment-LoadConfig-SnapshotTypes.sql
+
+--------------------------------------------------------------------------------------
+-- Load Default Exclusions
+--------------------------------------------------------------------------------------
+:r .\Scripts\Post-Deployment\Reference-Data\Script.PostDeployment-LoadConfig-DefaultExclusions.sql
+
+--------------------------------------------------------------------------------------
+-- DATA FIX: Se default Last Seen Dates When migrating from >2.0
+--------------------------------------------------------------------------------------
+:r .\Scripts\Post-Deployment\Data-Fixes\Script.PostDeployment-DataFix-SetDefaultLastSeenDates.sql
+
+--------------------------------------------------------------------------------------
+-- load default report styles:
+--------------------------------------------------------------------------------------
+:r .\Scripts\Post-Deployment\Reference-Data\Script.PostDeployment-CreateDefaultReportStyles.sql
+
+--------------------------------------------------------------------------------------
+-- Load Default Action Template
+--------------------------------------------------------------------------------------
+:r .\Scripts\Post-Deployment\Reference-Data\Script.PostDeployment-CreateDefaultActionTemplate.sql
+
+--------------------------------------------------------------------------------------
+-- Load Default actions that DO NOT call reports
+--------------------------------------------------------------------------------------
+:r .\Scripts\Post-Deployment\Reference-Data\Script.PostDeployment-CreateDefaultActions.sql
+--------------------------------------------------------------------------------------
+-- Load Default reports 
+--------------------------------------------------------------------------------------
+:r .\Scripts\Post-Deployment\Reference-Data\Script.PostDeployment-CreateDefaultReports.sql
+
+--------------------------------------------------------------------------------------
+-- Load actions that call reports we have just created
+--------------------------------------------------------------------------------------
+:r .\Scripts\Post-Deployment\Reference-Data\Script.PostDeployment-CreateDefaultReportActions.sql
+
+-------------------------------------------------------------------------------------
+-- Load default checks
+--------------------------------------------------------------------------------------
+:r .\Scripts\Post-Deployment\Reference-Data\Script.PostDeployment-CreateDefaultChecks.sql
+
+-------------------------------------------------------------------------------------
+-- Load Global Config
+-------------------------------------------------------------------------------------
+:r .\Scripts\Post-Deployment\Reference-Data\Script.PostDeployment-LoadConfig-Global.sql
+
+-------------------------------------------------------------------------------------
+-- start XES
+-------------------------------------------------------------------------------------
+exec [dbo].[usp_sqlwatch_internal_start_xes]
+
+-------------------------------------------------------------------------------------
+-- Make Constraints Trusted Again
+-------------------------------------------------------------------------------------
+:r .\Scripts\Post-Deployment\Data-Fixes\Script.PostDeployment-FixNonTrustedConstraints.sql
+
+-------------------------------------------------------------------------------------
+-- Migrate Data
+-------------------------------------------------------------------------------------
+:r .\Scripts\Post-Deployment\Data-Fixes\Script.PostDeployment-DataFix-MigrateReportTime.sql
+
+--------------------------------------------------------------------------------------
+-- populate central repository tables to import
+-- this will have no meaning on the remote instance but it will mean less steps
+-- when turning it into a central repository
+--------------------------------------------------------------------------------------
+--more than one record in the sql_instance table assumes we're running central repository:
+if (select count(*) from [dbo].[sqlwatch_config_sql_instance]) > 1
+	begin
+		Print 'Looks like this is a central repository so we''re going to populate tabels to import'
+		exec [dbo].[usp_sqlwatch_repository_populate_tables_to_import]
+	end
+
+-------------------------------------------------------------------------------------
+-- THIS MUST BE LAST STATEMENT IN THE PROCESS SO WE CAN RUN DATA-MIGRATIONS BASED
+-- ON THE CURRENT VERSION. IF WE UPDATE VERSION BEFORE RUNNING DATA MIGRATION IT WILL
+-- BE A DISASTER
+--------------------------------------------------------------------------------------
+insert into [dbo].[sqlwatch_app_version] ( [install_date], [sqlwatch_version] )
+values (SYSDATETIMEOFFSET(), RTRIM(LTRIM(REPLACE(REPLACE('$(DacVersion)',CHAR(10),''),CHAR(13),''))))
+
+--------------------------------------------------------------------------------------
+--setup jobs
+--we have to switch database to msdb but we also need to know which db jobs should run in so have to capture current database:
+--------------------------------------------------------------------------------------
+if (select case when @@VERSION like '%Express Edition%' then 1 else 0 end) = 0
+	begin
+		exec dbo.[usp_sqlwatch_config_create_default_agent_jobs]
+	end
---
+++ SqlWatch.Monitor/Project.SqlWatch.Database/Script.PreDeployment2.sql
@@ -0,0 +1,33 @@
+﻿/*
+ Pre-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be executed before the build script.	
+ Use SQLCMD syntax to include a file in the pre-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the pre-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+
+/* support for XE sessions in VS is dissapointing. VS will create new/missing session but will not alter existing.
+	To work around this we can drop sessions manually however, doing it here, in the pre-deployment script is too late.
+	At this point the deployment is planned and if we drop sessions now they will not be re-created. To work around this, 
+	there is similar step in Post-deployment script */
+--Print 'Stoppring SQLWATCH XE Sessions'
+--declare @sqlstmt varchar(4000) = ''
+
+--select @sqlstmt = @sqlstmt + 'DROP EVENT SESSION [' + name + '] ON SERVER;' + char(10) 
+--from sys.server_event_sessions 
+--where name in (	'SQLWATCH_blockers', 'SQLWATCH_waits','SQLWATCH_long_queries')
+
+--exec (@sqlstmt)
+
+
+
+:r .\Scripts\Pre-Deployment\SetDacVersion.sql
+
+declare @dacverion varchar(max)
+set @dacverion = '$(DacVersion)'
+
