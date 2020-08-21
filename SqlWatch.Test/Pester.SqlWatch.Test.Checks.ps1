d28dc133e6eaed09a15ac6bbe5ca1b66b9060d53
Marcin Gminski <marcingminski@gmail.com>
2020-08-21 15:57:16 +0100

    New tests

--- SqlWatch.Test/Pester.SqlWatch.Test.Checks.ps1
+++ SqlWatch.Test/Pester.SqlWatch.Test.Checks.ps1
@@ -1,7 +1,8 @@
 ﻿param(
     $SqlInstance,
     $SqlWatchDatabase,
-    $MinSqlUpHours = 4
+    $MinSqlUpHours,
+    $LookBackHours
 )
 
 $sql = "select datediff(hour,sqlserver_start_time,getdate()) from sys.dm_os_sys_info"
@@ -37,7 +38,7 @@ Describe 'Failed Checks' {
                     from [dbo].[sqlwatch_config_check] cc 
                     left join [dbo].[sqlwatch_logger_check] lc 
                         on cc.check_id = lc.check_id 
-                    where snapshot_time > dateadd(hour,-48,getutcdate()) 
+                    where snapshot_time > dateadd(hour,-$($LookBackHours),getutcdate()) 
                     and cc.check_name = '$($check_name)' 
                     and lc.check_status like '%ERROR%'
                     --skip records after restart as Perf counters will be empty right after restart.
@@ -95,20 +96,27 @@ Describe 'Failed Checks' {
             from [dbo].[sqlwatch_logger_check] lc
             inner join dbo.sqlwatch_config_check cc
 	            on lc.check_id = cc.check_id
-            where snapshot_time > dateadd(hour,-48,getutcdate())
+            where snapshot_time > dateadd(hour,-$($LookBackHours),getutcdate())
             and cc.check_name = '$($check_name)'
 
             create clustered index icx_tmp_t on #t (snapshot_time)
 
             select
-	            min_check_frequency_minutes_calculated=min(datediff(minute,c1.snapshot_time,c2.snapshot_time))
+	            min_check_frequency_minutes_calculated=ceiling(avg(datediff(second,c1.snapshot_time,c2.snapshot_time)/60.0))
             from #t c1
             left join #t c2
 	            on c1.check_id = c2.check_id
 	            and c1.RN = c2.RN -1
             "
+
+                # Because sql agent is syncronous, there is no guarantee that checks will always run at the same interval. There will be +/- few seconds or even a couple of minutes
+                # If the check takes longer to execute, the gap between the second execution will be smaller than it should be. 
+                # We are going to allow some deviation of +/- 1 minute
+                $minvalue=$check_frequency_minutes.check_frequency_minutes-1
+                $maxvalue=$check_frequency_minutes.check_frequency_minutes+1
+                $value=$check_frequency_minutes.check_frequency_minutes
                 $result = Invoke-SqlCmd -ServerInstance $SqlInstance -Database $SqlWatchDatabase -Query $sql
-                $result.min_check_frequency_minutes_calculated | Should -Be $check_frequency_minutes.check_frequency_minutes -Because 'The agent job that invokes the check engine runs every 1 minute but not all checks should run every 1 minute. There is a [check_frequency_minutes] column that tells us how often each check should run. This value must be respected otherwise all checks will run every 1 minute which will create additional load on the server'
+                $result.min_check_frequency_minutes_calculated | Should -BeIn ($minvalue,$value,$maxvalue) -Because 'The agent job that invokes the check engine runs every 1 minute but not all checks should run every 1 minute. There is a [check_frequency_minutes] column that tells us how often each check should run. This value must be respected otherwise all checks will run every 1 minute which will create additional load on the server'
 
             }
           }
@@ -139,7 +147,136 @@ Describe 'Test database design' {
             $result = Invoke-SqlCmd -ServerInstance $SqlInstance -Database $SqlWatchDatabase -Query $sql;
 
             $sql = "select top 1 datediff(hour,GETUTCDATE(),$($result.COLUMN_NAME)) from [$($result.$TABLE_NAME)] order by $($result.COLUMN_NAME) desc";
-            $result.Column1 | should -Not -BeGreaterThan 0 'Values in the future indicate incorrect time zone'    
+            $result.Column1 | should -Not -BeGreaterThan 0 -Because 'Values in the future indicate incorrect time zone'    
         }
+    }
+}
+
+Describe 'Test Check Results' {
+
+    It 'The Latest Data Backup Check should return correct result' {
+        ## Use dbatools as a baseline
+        $LastFullBackup = Get-DbaLastBackup -SqlInstance $SqlInstance | Sort-Object LastFullBackup -Descending | Select-Object -first 1 
+        $LastDiffBackup = Get-DbaLastBackup -SqlInstance $SqlInstance | Sort-Object LastDiffBackup -Descending | Select-Object -first 1 
+
+        $LastFullBackupAgeDays = $(New-TimeSpan -Start $LastFullBackup.LastFullBackup -End (Get-Date)).TotalDays
+        $LastDiffBackupAgeDays = $(New-TimeSpan -Start $LastDiffBackup.LastDiffBackup -End (Get-Date)).TotalDays        
+
+        if ($LastDiffBackupAgeDays -lt $LastFullBackupAgeDays) {
+            [int]$LastDataBackupAgeDays = $LastDiffBackupAgeDays
+        } else {
+            [int]$LastDataBackupAgeDays = $LastFullBackupAgeDays
+        }
+
+        ## this is the sql query from the check itself as we have to compare live values:
+        ## check_id -18        
+        $sql = "select check_query from dbo.sqlwatch_meta_check where check_id = -18"
+        $result = Invoke-SqlCmd -ServerInstance $SqlInstance -Database $SqlWatchDatabase -Query $sql;
+
+        $sql = $result.check_query -replace "@output","output"
+        $result = Invoke-SqlCmd -ServerInstance $SqlInstance -Database $SqlWatchDatabase -Query $sql;
+
+        $result.output | should -Be $LastDataBackupAgeDays 
+    }
+
+    It 'The Latest Log Backup Check should return correct result' {
+        ## Use dbatools as a baseline
+        $LastLogBackup = Get-DbaLastBackup -SqlInstance SQL-1 | Sort-Object LastLogBackup -Descending | Select-Object -first 1 
+
+        [int]$LastLogBackupAgeMinutes = $(New-TimeSpan -Start $LastLogBackup.LastLogBackup -End (Get-Date)).TotalMinutes
+
+        ## this is the sql query from the check itself as we have to compare live values:
+        ## check_id -17
+        $sql = "select check_query from dbo.sqlwatch_meta_check where check_id = -17"
+        $result = Invoke-SqlCmd -ServerInstance $SqlInstance -Database $SqlWatchDatabase -Query $sql;
+
+        $sql = $result.check_query -replace "@output","output"
+        $result = Invoke-SqlCmd -ServerInstance $SqlInstance -Database $SqlWatchDatabase -Query $sql;
+
+        $result.output | should -Be $LastLogBackupAgeMinutes 
+    }
+}
+
+Describe 'Data Retention' {
+
+    Context 'Checking Snapshot Retention Policy is being applied' {
+
+        $SnapshotTypes = Invoke-Sqlcmd -ServerInstance $SqlInstance -Database $SqlWatchDatabase -Query "select * from sqlwatch_config_snapshot_type"
+
+        $TestCases = @();
+        $SnapshotTypes.ForEach{$TestCases += @{SnapshotTypeDesc = $_.snapshot_type_desc }}
+    
+        It 'Snapshot Type [<SnapshotTypeDesc>] should respect retention policy' -TestCases $TestCases {
+            Param($SnapshotTypeDesc)
+    
+            $sql = "select count(*)
+            from sqlwatch_logger_snapshot_header h
+            inner join sqlwatch_config_snapshot_type t
+                on h.snapshot_type_id = t.snapshot_type_id
+            where datediff(day,h.snapshot_time,getutcdate()) > snapshot_retention_days
+            and snapshot_type_desc = '$($SnapshotTypeDesc)'
+            and snapshot_retention_days > 0"
+    
+            $result = Invoke-SqlCmd -ServerInstance $SqlInstance -Database $SqlWatchDatabase -Query $sql
+            $result.Column1 | Should -Be 0 -Because "There should not be any rows beyond the max age."
+        }
+
+    }
+
+    Context 'Checking Last Seen Retention Policy is being applied' {
+
+        $sql = "select TABLE_SCHEMA + '.' + TABLE_NAME
+        from INFORMATION_SCHEMA.TABLES
+        where TABLE_NAME IN (
+            select DISTINCT TABLE_NAME
+            from INFORMATION_SCHEMA.COLUMNS
+            where COLUMN_NAME = 'date_last_seen'
+        )
+        and TABLE_TYPE = 'BASE TABLE'";
+    
+        $Tables = Invoke-Sqlcmd -ServerInstance $SqlInstance -Database $SqlWatchDatabase -Query $sql
+    
+        $TestCases = @();
+        $Tables.ForEach{$TestCases += @{TableName = $_.Column1 }}
+
+        It 'The "Last Seen" Retention in Table [<TableName>] should respect the configuration setting' -TestCases $TestCases {
+
+            Param($TableName)
+            
+            $sql = "select count(*) from $($TableName) where datediff(day,date_last_seen,getutcdate()) > [dbo].[ufn_sqlwatch_get_config_value](2,null)"
+            $result = Invoke-SqlCmd -ServerInstance $SqlInstance -Database $SqlWatchDatabase -Query $sql
+            $result.Column1 | Should -Be 0 -Because "There should not be any rows beyond the max age." 
+        }
+    }
+}
+
+Describe 'Application Errors' {
+
+    Context 'Check Application log for WARNINGS' {
+
+        <# Warnings are OK. Flapping checks or exceeding email thresholds etc.
+        It 'Application Log should not contain WARNINGS' {
+
+            $sql = "select count(*) from [dbo].[sqlwatch_app_log]
+            where process_message_type = 'WARNING'
+            and event_sequence > dateadd(hour,-$($LookBackHours),getutcdate())"
+
+            $result = Invoke-SqlCmd -ServerInstance $SqlInstance -Database $SqlWatchDatabase -Query $sql
+            $result.Column1 | Should -Be 0
+
+        }
+        #>
+        
+        It 'Application Log should not contain ERRORS' {
+
+            $sql = "select count(*) from [dbo].[sqlwatch_app_log]
+            where process_message_type = 'ERRROR'
+            and event_sequence > dateadd(hour,-$($LookBackHours),getutcdate())"
+
+            $result = Invoke-SqlCmd -ServerInstance $SqlInstance -Database $SqlWatchDatabase -Query $sql
+            $result.Column1 | Should -Be 0
+
+        }
+
     }
 }
