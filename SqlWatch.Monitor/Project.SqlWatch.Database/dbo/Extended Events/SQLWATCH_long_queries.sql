737d976e524496ff049938c5d8d166cb8681d6b7
Marcin Gminski <marcingminski@gmail.com>
2020-08-26 21:43:52 +0100

    fixed file size

--- SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Extended Events/SQLWATCH_long_queries.sql
+++ SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Extended Events/SQLWATCH_long_queries.sql
@@ -24,6 +24,6 @@ ADD EVENT sqlserver.sql_statement_completed(
     ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_name,sqlserver.plan_handle,sqlserver.session_id,sqlserver.session_nt_username,sqlserver.sql_text,sqlserver.tsql_stack,sqlserver.username)
     WHERE ([package0].[greater_than_int64]([duration],(15000000)) AND ([package0].[greater_than_uint64]([cpu_time],(0)) OR [package0].[greater_than_uint64]([logical_reads],(0)) OR [package0].[greater_than_uint64]([physical_reads],(0)) OR [package0].[greater_than_uint64]([writes],(0))) AND [sqlserver].[is_system]=(0)))
 --ADD TARGET package0.ring_buffer(SET max_events_limit=(250))
-ADD TARGET package0.event_file(SET filename='SQLWATCH_long_queries.xel', max_file_size=(1), max_rollover_files=(0))
+ADD TARGET package0.event_file(SET filename='SQLWATCH_long_queries.xel', max_file_size=(5), max_rollover_files=(0))
 WITH (MAX_MEMORY=4096 KB,EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS,MAX_DISPATCH_LATENCY=5 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=NONE,TRACK_CAUSALITY=ON,STARTUP_STATE=ON)
 GO
