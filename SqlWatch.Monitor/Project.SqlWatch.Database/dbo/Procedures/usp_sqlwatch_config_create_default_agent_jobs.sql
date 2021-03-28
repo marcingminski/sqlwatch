5efe86d792a1903d2171f7d054be84af0e9375ef
Marcin Gminski <marcingminski@gmail.com>
2021-03-28 19:48:26 +0100

    separated xes collectors into their own jobs and change default performance counter collector to run every 10 seconds

--- SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Procedures/usp_sqlwatch_config_create_default_agent_jobs.sql
+++ SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Procedures/usp_sqlwatch_config_create_default_agent_jobs.sql
@@ -4,31 +4,6 @@ CREATE PROCEDURE [dbo].[usp_sqlwatch_config_create_default_agent_jobs]
 	@job_owner sysname = null
 AS
 
-/*
--------------------------------------------------------------------------------------------------------------------
- Procedure:
-	usp_sqlwatch_config_set_default_agent_jobs
-
- Description:
-	Creates default SQLWATCH Agent jobs. This procedure is triggered via post-deploy script and creates all SQLWATCH
-	jobs during the initial deployment. Once created, the jobs will never be modified during subsequent upgrades. 
-	This is to prevent overwriting user-defined schedules. To overwrite job i.e. to fix a bug delete existing job first.
-	Alternatively, parameter @remove_existing can be used to delete jobs automatically. This will also delete jobs' history.
-
- Parameters
-	@remove_existing	-	Force delete jobs so they can be re-created.
-	@print_WTS_command	-	Print Command to create equivalent tasks in Windows Task scheduler for editions that have no
-							SQL Agent i.e. Express.
-	
- Author:
-	Marcin Gminski
-
- Change Log:
-	1.0		2019-xx-xx	- Marcin Gminski, Initial version
-	1.1		2019-12-04	- Marcin Gminski, Print command to create Windows Scheduled Tasks for editions without agent
-	1.2		2020-01-29	- Marcin Gminski, add job owner
--------------------------------------------------------------------------------------------------------------------
-*/
 set nocount on;
 
 /* create jobs */
@@ -84,7 +59,9 @@ insert into ##sqlwatch_jobs
 	values	
 			('SQLWATCH-INTERNAL-CONFIG',		4,			1,				8,					1,						0,						1,							20180101,			99991231,			26,					235959,				1),
 
-			('SQLWATCH-LOGGER-PERFORMANCE',		4,			1,				4,					1,						0,						1,							20180101,			99991231,			12,					235959,				1),
+			('SQLWATCH-LOGGER-PERFORMANCE',		4,			1,				2,					10,						0,						1,							20180101,			99991231,			12,					235959,				1),
+			('SQLWATCH-LOGGER-XES',				4,			1,				4,					1,						0,						1,							20180101,			99991231,			12,					235959,				1),
+
 			('SQLWATCH-LOGGER-DISK-UTILISATION',4,			1,				8,					1,						0,						1,							20180101,			99991231,			437,				235959,				1),
 			('SQLWATCH-LOGGER-INDEXES',			4,			1,				1,					24,						0,						1,							20180101,			99991231,			1500,				235959,				1),
 			('SQLWATCH-LOGGER-AGENT-HISTORY',	4,			1,				4,					10,						0,						1,							20180101,			99991231,			0,					235959,				1),
@@ -117,10 +94,10 @@ insert into ##sqlwatch_steps
 	values	('dbo.usp_sqlwatch_logger_whoisactive',		1,			'SQLWATCH-LOGGER-WHOISACTIVE',		'TSQL',		'exec dbo.usp_sqlwatch_logger_whoisactive'),
 
 			('dbo.usp_sqlwatch_logger_performance',		1,			'SQLWATCH-LOGGER-PERFORMANCE',		'TSQL',		'exec dbo.usp_sqlwatch_logger_performance'),
-			('dbo.usp_sqlwatch_logger_xes_waits',		2,			'SQLWATCH-LOGGER-PERFORMANCE',		'TSQL',		'exec dbo.usp_sqlwatch_logger_xes_waits'),
-			('dbo.usp_sqlwatch_logger_xes_blockers',	3,			'SQLWATCH-LOGGER-PERFORMANCE',		'TSQL',		'exec dbo.usp_sqlwatch_logger_xes_blockers'),
-			('dbo.usp_sqlwatch_logger_xes_diagnostics',	4,			'SQLWATCH-LOGGER-PERFORMANCE',		'TSQL',		'exec dbo.usp_sqlwatch_logger_xes_diagnostics'),
-			('dbo.usp_sqlwatch_logger_xes_long_queries',5,			'SQLWATCH-LOGGER-PERFORMANCE',		'TSQL',		'exec dbo.usp_sqlwatch_logger_xes_long_queries'),
+			('dbo.usp_sqlwatch_logger_xes_waits',		1,			'SQLWATCH-LOGGER-XES',				'TSQL',		'exec dbo.usp_sqlwatch_logger_xes_waits'),
+			('dbo.usp_sqlwatch_logger_xes_blockers',	2,			'SQLWATCH-LOGGER-XES',				'TSQL',		'exec dbo.usp_sqlwatch_logger_xes_blockers'),
+			('dbo.usp_sqlwatch_logger_xes_diagnostics',	3,			'SQLWATCH-LOGGER-XES',				'TSQL',		'exec dbo.usp_sqlwatch_logger_xes_diagnostics'),
+			('dbo.usp_sqlwatch_logger_xes_long_queries',4,			'SQLWATCH-LOGGER-XES',				'TSQL',		'exec dbo.usp_sqlwatch_logger_xes_long_queries'),
 
 			('dbo.usp_sqlwatch_trend_perf_os_performance_counters',1,'SQLWATCH-INTERNAL-TRENDS',		'TSQL',		'exec dbo.usp_sqlwatch_trend_perf_os_performance_counters'),
 
