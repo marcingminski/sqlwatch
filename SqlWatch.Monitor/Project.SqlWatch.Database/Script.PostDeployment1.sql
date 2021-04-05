c428a028258853b60dbb6d02a4a31e7c36ec2f67
Marcin Gminski <marcingminski@gmail.com>
2021-04-05 17:50:11 +0100

    added proc to restart queues. should never be required but good to have just in case

--- SqlWatch.Monitor/Project.SqlWatch.Database/Script.PostDeployment1.sql
+++ SqlWatch.Monitor/Project.SqlWatch.Database/Script.PostDeployment1.sql
@@ -120,40 +120,6 @@ if (select case when @@VERSION like '%Express Edition%' then 1 else 0 end) = 0
 :r .\Scripts\Post-Deployment\Data-Fixes\Script.PostDeployment-FixNonTrustedConstraints.sql
 
 -------------------------------------------------------------------------------------
--- Begin conversations
+-- Retart queues
 ------------------------------------------------------------------------------------- 
-    --stop and clean all sqlwatch conversations in the database
-    --this needs improvements as we shuold not be cleaning up existing conversations.
-    set @sql = ''
-    select @sql = @sql + '
-    end conversation ''' + convert(varchar(max),conversation_handle) + ''' WITH CLEANUP;'
-    from sys.conversation_endpoints
-    where far_service like 'sqlwatch%'
-
-    exec (@sql)
-
-    declare @conversation_handle uniqueidentifier;
-
-    begin dialog conversation @conversation_handle
-        from service sqlwatch_invoke_5s
-        to service N'sqlwatch_invoke_5s', N'current database'
-        with encryption = off;
-    begin conversation timer (@conversation_handle) timeout = 5;
-
-    begin dialog conversation @conversation_handle
-        from service sqlwatch_invoke_1m
-        to service N'sqlwatch_invoke_1m', N'current database'
-        with encryption = off;
-    begin conversation timer (@conversation_handle) timeout = 60;
-
-    begin dialog conversation @conversation_handle
-        from service sqlwatch_invoke_10m
-        to service N'sqlwatch_invoke_10m', N'current database'
-        with encryption = off;
-    begin conversation timer (@conversation_handle) timeout = 600;
-
-    begin dialog conversation @conversation_handle
-        from service sqlwatch_invoke_60m
-        to service N'sqlwatch_invoke_60m', N'current database'
-        with encryption = off;
-    begin conversation timer (@conversation_handle) timeout = 3600;
+exec [dbo].[usp_sqlwatch_internal_restart_queues]
