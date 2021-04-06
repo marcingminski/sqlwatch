2bae25197f7bd7b47351d418d47f7c7cf84597bb
Marcin Gminski <marcingminski@gmail.com>
2021-04-06 14:47:12 +0100

    helper to covnert datetimeoffset to local time

---
+++ SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Functions/ufn_sqlwatch_convert_datetimeoffset_to_local.sql
@@ -0,0 +1,11 @@
+﻿CREATE FUNCTION [dbo].[ufn_sqlwatch_convert_datetimeoffset_to_local] 
+(
+	@datetimeoffset datetimeoffset
+)
+
+returns datetime2(0) 
+with schemabinding
+as
+begin
+	return convert(datetime, switchoffset(convert(datetime, @datetimeoffset), datename(TzOffset, @datetimeoffset)))
+end
