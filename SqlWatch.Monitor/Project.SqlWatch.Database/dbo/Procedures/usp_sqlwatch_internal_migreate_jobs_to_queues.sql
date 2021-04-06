f50f9f714e1ede31bf736fe6ddcf738c0387627a
Marcin Gminski <marcingminski@gmail.com>
2021-04-06 21:56:21 +0100

    jobs to broker migration

--- SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Procedures/usp_sqlwatch_internal_migreate_jobs_to_queues.sql
+++ SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Procedures/usp_sqlwatch_internal_migreate_jobs_to_queues.sql
@@ -2,6 +2,20 @@
 as
 
 --this procedure will disable and remove the relevent agent jobs and enable broker based collection
--- to do
+declare @sql varchar(max) = '',
+		@database_name sysname = db_name()
 
-return 'Work in progress...'
+select @sql = @sql  + ';' + char(10) + 'exec msdb.dbo.sp_delete_job @job_id=N'''+ convert(varchar(255),job_id) +''', @delete_unused_schedule=1' 
+from msdb.dbo.sysjobs
+where name like 'SQLWATCH-\[' + @database_name + '\]%' ESCAPE '\'
+and name not like '%AZMONITOR'
+and name not like '%ACTIONS'
+and name not like '%DISK-UTILISATION'
+and name not like '%INDEXES'
+and name not like '%WHOISACTIVE'
+
+exec (@sql);
+
+
+--activate queues:
+exec [dbo].[usp_sqlwatch_internal_restart_queues];
