5285e2591a861ed626f654e92eafe1d15d41fecb
Marcin Gminski <marcingminski@gmail.com>
2021-04-06 21:34:05 +0100

    added placeholder for broker migration

---
+++ SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Procedures/usp_sqlwatch_internal_migreate_jobs_to_queues.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[usp_sqlwatch_internal_migreate_jobs_to_queues]
+as
+
+--this procedure will disable and remove the relevent agent jobs and enable broker based collection
+-- to do
+
+return 'Work in progress...'
