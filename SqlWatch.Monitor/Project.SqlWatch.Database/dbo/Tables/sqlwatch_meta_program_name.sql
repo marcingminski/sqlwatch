ebc28c93e3f07f30cc56ed484e673b0ab754123d
Marcin Gminski <marcingminski@gmail.com>
2021-04-10 22:16:36 +0100

    dimension table for program names

---
+++ SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Tables/sqlwatch_meta_program_name.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[sqlwatch_meta_program_name]
+(
+	[program_name_id] smallint identity(1,1) not null,
+	[program_name] nvarchar(128),
+	[sql_instance] varchar(32),
+
+	constraint pk_sqlwatch_meta_program_name 
+		primary key clustered (
+		  [program_name]
+		, [sql_instance]
+		),
+
+	constraint fk_sqlwatch_meta_program_name_sql_instance 
+		foreign key ([sql_instance])
+		references [dbo].[sqlwatch_meta_server] ([servername]) 
+		on delete cascade
+)
+go
+
+create unique index idx_sqlwatch_meta_program_name_id 
+	on [dbo].[sqlwatch_meta_program_name] ([program_name_id])
