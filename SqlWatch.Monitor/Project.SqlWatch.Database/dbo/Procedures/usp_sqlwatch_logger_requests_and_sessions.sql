6501e8bfdbc02900b13ef46f5f1203f7b9faf475
Marcin Gminski <marcingminski@gmail.com>
2021-04-10 22:20:56 +0100

    not entirely happy with this approach so disabling for now

--- SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Procedures/usp_sqlwatch_logger_requests_and_sessions.sql
+++ SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Procedures/usp_sqlwatch_logger_requests_and_sessions.sql
@@ -1,10 +1,54 @@
 ﻿CREATE PROCEDURE [dbo].[usp_sqlwatch_logger_requests_and_sessions]
 as
 
+	/*
+
+	I am not entirely happy with this approach....
+
 	declare @snapshot_type_id tinyint = 30,
 			@date_snapshot_current datetime2(0),
 			@sql_instance varchar(32) = dbo.ufn_sqlwatch_get_servername();
 
+	declare @requests table (
+		[session_id] [smallint] NOT NULL,
+		[start_time] [datetime] NOT NULL,
+		[status] [nvarchar](30) NOT NULL,
+		[command] [nvarchar](32) NOT NULL,
+		[sql_handle] [varbinary](64) NULL,
+		[plan_handle] [varbinary](64) NULL,
+		[database_name] [sysname] NOT NULL,
+		[blocking_session_id] [smallint] NULL,
+		[wait_type] [nvarchar](60) NULL,
+		[wait_time] [int] NOT NULL,
+		[text] [nvarchar](max) NULL,
+		[program_name] [nvarchar](128) NULL,
+		[client_interface_name] [nvarchar](32) NULL,
+		[host_name] [nvarchar](128) NULL,
+		[login_name] [nvarchar](128) NULL
+	);
+
+	insert into @requests
+	select 
+		session_id
+		, start_time
+		, status
+		, command
+		, sql_handle
+		, plan_handle
+		, sqlwatch_database_id
+		, blocking_session_id
+		, wait_type 
+		, wait_time
+	from sys.dm_exec_requests r
+	
+	inner join sys.databases db
+	on db.database_id = r.database_id
+	
+	inner join dbo.sqlwatch_meta_database sdb
+	on sdb.database_name = db.name
+	and sdb.database_create_date = case when db.name = 'tempdb' then '1970-01-01' else db.create_date end
+
+	;
 	exec [dbo].[usp_sqlwatch_internal_insert_header] 
 		@snapshot_time_new = @date_snapshot_current OUTPUT,
 		@snapshot_type_id = @snapshot_type_id;
@@ -69,3 +113,4 @@ as
 	on s.database_id = db.database_id
 	where session_id > 50
 	option (keep plan);
+	*/
