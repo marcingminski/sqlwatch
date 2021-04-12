c7361a98d9bc122ea9876ad0582e6a98c84740bb
Marcin Gminski <marcingminski@gmail.com>
2021-04-12 20:17:37 +0100

    increased the max file to 15MB as query plans can be large

--- SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Extended Events/SQLWATCH_long_queries.sql
+++ SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Extended Events/SQLWATCH_long_queries.sql
@@ -28,6 +28,6 @@ ADD EVENT sqlserver.query_post_execution_showplan(SET collect_database_name=(1)
     ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_name,sqlserver.session_id,sqlserver.session_nt_username,sqlserver.sql_text,sqlserver.tsql_stack,sqlserver.username)
     WHERE ([package0].[greater_than_uint64]([duration],(5000000))))
 
-ADD TARGET package0.event_file(SET filename='SQLWATCH_long_queries.xel', max_file_size=(5), max_rollover_files=(0))
+ADD TARGET package0.event_file(SET filename='SQLWATCH_long_queries.xel', max_file_size=(15), max_rollover_files=(0))
 WITH (MAX_MEMORY=4096 KB,EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS,MAX_DISPATCH_LATENCY=5 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=NONE,TRACK_CAUSALITY=ON,STARTUP_STATE=ON)
 GO
