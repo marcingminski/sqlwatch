648c7f4c7aa75b3ca60232b9ecbb124cf4a91094
Marcin Gminski <marcingminski@gmail.com>
2021-04-14 15:30:06 +0100

    fixed data conversion problems introduced by earlier checkin - caused by stupid code cleanup where I decided to rearrange case when, not taking into consideration that the order is important!

--- SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Functions/ufn_sqlwatch_get_check_status.sql
+++ SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Functions/ufn_sqlwatch_get_check_status.sql
@@ -18,12 +18,6 @@ BEGIN
 					case
 						when @value <= convert(decimal(28,5),replace(@threshold,'<=','')) * @variance_percent_dec_min
 						then 1 else 0 end
-
-				-- less than
-				when left(@threshold,1) = '<' then
-					case 
-						when @value < convert(decimal(28,5),replace(@threshold,'<','')) * @variance_percent_dec_min
-						then 1 else 0 end
 					
 				-- greater or equal
 				when left(@threshold,2) = '>=' then
@@ -31,15 +25,21 @@ BEGIN
 						when @value >= convert(decimal(28,5),replace(@threshold,'>=','')) * @variance_percent_dec_max
 						then 1 else 0 end
 				
+				-- exact mismatch -- no variance
+				when left(@threshold,2) = '<>' then
+					case when @value <> convert(decimal(28,5),replace(@threshold,'<>','')) then 1 else 0 end
+
+				-- less than
+				when left(@threshold,1) = '<' then
+					case 
+						when @value < convert(decimal(28,5),replace(@threshold,'<','')) * @variance_percent_dec_min
+						then 1 else 0 end
+
 				-- greater than
 				when left(@threshold,1) = '>' then
 					case 
 						when @value > convert(decimal(28,5),replace(@threshold,'>','')) * @variance_percent_dec_max
 						then 1 else 0 end
-				
-				-- exact mismatch -- no variance
-				when left(@threshold,2) = '<>' then
-					case when @value <> convert(decimal(28,5),replace(@threshold,'<>','')) then 1 else 0 end
 		
 				-- exact match -- no variance
 				when left(@threshold,1) = '=' then
