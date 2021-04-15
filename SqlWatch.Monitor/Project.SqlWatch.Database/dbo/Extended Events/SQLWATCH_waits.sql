5b9ddd872e08be856dcfdfa87f927328c01d850c
Marcin Gminski <marcingminski@gmail.com>
2021-04-15 19:57:35 +0100

    baselining for 2014

--- SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Extended Events/SQLWATCH_waits.sql
+++ SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Extended Events/SQLWATCH_waits.sql
@@ -4,9 +4,9 @@ ON SERVER
 /*	any query that waited over 1 second for a resource
 	Remove when targeting SQL2008	*/
 
-ADD EVENT sqlos.wait_completed(SET collect_wait_resource=(1)
-    ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_name,sqlserver.query_hash,sqlserver.query_hash_signed,sqlserver.session_id,sqlserver.session_nt_username,sqlserver.sql_text,sqlserver.username)
-    WHERE ([package0].[greater_than_uint64]([duration],(1000)) AND [sqlserver].[not_equal_i_sql_unicode_string]([sqlserver].[sql_text],N''))),
+--ADD EVENT sqlos.wait_completed(SET collect_wait_resource=(1)
+--    ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_name,sqlserver.query_hash,sqlserver.query_hash_signed,sqlserver.session_id,sqlserver.session_nt_username,sqlserver.sql_text,sqlserver.username)
+--    WHERE ([package0].[greater_than_uint64]([duration],(1000)) AND [sqlserver].[not_equal_i_sql_unicode_string]([sqlserver].[sql_text],N''))),
 ADD EVENT sqlserver.sp_statement_completed(SET collect_statement=(1)
     ACTION(sqlserver.query_hash)
     WHERE ([package0].[greater_than_int64]([duration],(1000000)) AND [sqlserver].[query_hash]>(0))),
