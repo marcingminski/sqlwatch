58bd2844f96125925ba556b71b5654f735f48100
Marcin Gminski <marcingminski@gmail.com>
2021-04-16 21:01:17 +0100

    read performance categories. This will return all perf counters from the os and is not meant to run often. perhaps only during the installation or once a day. It takes around 20 seconds to run.

---
+++ SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Functions/CLR/ReadPerformanceCounterCategories.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Diagnostics;
+using Microsoft.SqlServer.Server;
+
+public partial class UserDefinedFunctions
+{
+
+    private class CounterProperties
+    {
+        public SqlString objectName;
+        public SqlString counterName;
+        public SqlString instanceName;
+        public CounterProperties(String objectName, String counterName, String instanceName)
+        {
+            this.objectName = objectName;
+            this.counterName = counterName;
+            this.instanceName = instanceName;
+        }
+    }
+
+
+
+    [Microsoft.SqlServer.Server.SqlFunction(
+         FillRowMethodName = "GetCounters",
+         TableDefinition = "object_name nvarchar(128), counter_name nvarchar(128), instance_name nvarchar(128)")]
+    public static IEnumerable ReadPerformanceCounterCategories()
+    {
+       PerformanceCounterCategory[] categories = PerformanceCounterCategory.GetCategories();
+
+       ArrayList performanceCounterCollection = new ArrayList();
+
+       foreach (PerformanceCounterCategory category in categories)
+       {
+            string[] instanceNames = category.GetInstanceNames();
+            if (instanceNames.Length > 0)
+            {
+                // MultiInstance counters
+                foreach (string instanceName in instanceNames)
+                {
+                    PerformanceCounter[] counters = category.GetCounters(instanceName);
+                    foreach (PerformanceCounter counter in counters)
+                    {
+                        performanceCounterCollection.Add(new CounterProperties(counter.CounterName, category.CategoryName, instanceName));
+                    }
+                }
+
+            }
+            else
+            {
+                PerformanceCounter[] counters = category.GetCounters();
+                foreach (PerformanceCounter counter in counters)
+                {
+                    performanceCounterCollection.Add(new CounterProperties(counter.CounterName, category.CategoryName, string.Empty));
+                }
+                
+            }
+       }
+
+        return performanceCounterCollection;
+    }
+
+    public static void GetCounters(object objCounterProperties,out SqlChars object_name, out SqlChars counter_name, out SqlChars instance_name)
+    {
+        CounterProperties counterProperties = (CounterProperties)objCounterProperties;
+        object_name = new SqlChars(counterProperties.objectName);
+        counter_name = new SqlChars(counterProperties.counterName);
+        instance_name = new SqlChars(counterProperties.instanceName);
+    }
+}
--- SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Functions/CLR/ReadPerformanceCounterFormatted.cs
+++ SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Functions/CLR/ReadPerformanceCounterFormatted.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Data.SqlTypes;
 using System.Diagnostics;
 
@@ -8,6 +9,7 @@ public partial class UserDefinedFunctions
     {
         try
         {
+
             PerformanceCounter counterValue = new PerformanceCounter(objectName, counterName, instanceName);
 
             float returnValue = counterValue.NextValue();
@@ -17,11 +19,12 @@ public partial class UserDefinedFunctions
                 //https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.performancecounter
                 //For some counters, the NextValue will always return 0 on the first iteration as its a calculated value, it needs a value to compare against:
                 //Before we check it again, we're putting it to sleep to let it calm down after invocation:
-                System.Threading.Thread.Sleep(20);
+                System.Threading.Thread.Sleep(2);
                 returnValue = counterValue.NextValue();
 
             }
-            //bummer as SqlDouble (float) is non-nullable so if we ask to return a nonexistent counter, we cannot return null
+            //bummer as SqlDouble (float) is non-nullable so if we ask to return a nonexistent counter, we cannot return null.
+            //If we do not do try catch and ask for nonexistent counter the .NET will return a hard error and break the batch.
             //To return null we need a nullable type that will accept floating point, so string:
             return returnValue.ToString();
         }
