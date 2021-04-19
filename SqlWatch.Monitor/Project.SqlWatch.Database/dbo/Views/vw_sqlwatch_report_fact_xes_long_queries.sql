ac00127fc8227a3ccd22b73f2cc5549d951c3ed9
Marcin Gminski <marcingminski@gmail.com>
2021-04-19 23:15:53 +0100

    columns for grafana

--- SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Views/vw_sqlwatch_report_fact_xes_long_queries.sql
+++ SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Views/vw_sqlwatch_report_fact_xes_long_queries.sql
@@ -27,8 +27,19 @@ SELECT
  --for backward compatibility with existing pbi, this column will become report_time as we could be aggregating many snapshots in a report_period
 , d.snapshot_time
 , d.snapshot_type_id
+
+    , qp.query_plan
+    , qt.sql_text
   FROM [dbo].[sqlwatch_logger_xes_long_queries] d
   	inner join dbo.sqlwatch_logger_snapshot_header h
 		on  h.snapshot_time = d.[snapshot_time]
 		and h.snapshot_type_id = d.snapshot_type_id
 		and h.sql_instance = d.sql_instance
+
+    left join dbo.sqlwatch_meta_query_plan qp
+        on qp.sql_instance = d.sql_instance
+        and qp.sqlwatch_query_plan_id = d.sqlwatch_query_plan_id
+
+    left join dbo.sqlwatch_meta_query_text qt
+        on qt.sql_instance = d.sql_instance
+        and qt.sqlwatch_query_id = d.sqlwatch_query_id
