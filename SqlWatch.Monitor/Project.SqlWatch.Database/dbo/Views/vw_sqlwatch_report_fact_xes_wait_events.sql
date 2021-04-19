b59f0952aa9e5d14284a5f84f368321684668cc8
Marcin Gminski <marcingminski@gmail.com>
2021-04-19 22:04:21 +0100

    grafana dashboard to get details about wait events

--- SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Views/vw_sqlwatch_report_fact_xes_wait_events.sql
+++ SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Views/vw_sqlwatch_report_fact_xes_wait_events.sql
@@ -17,6 +17,10 @@ select e.[event_time]
       ,e.[sql_instance]
       ,e.[snapshot_time]
       ,e.[snapshot_type_id]
+      ,e.sqlwatch_query_id
+      ,e.sqlwatch_query_plan_id
+      ,p.query_plan
+      ,qt.sql_text
   from [dbo].[sqlwatch_logger_xes_wait_event] e
 
   inner join dbo.vw_sqlwatch_meta_wait_stats_category ws
@@ -31,6 +35,6 @@ select e.[event_time]
     on p.sqlwatch_query_plan_id = e.sqlwatch_query_plan_id
     and p.sql_instance = e.sql_instance
 
- -- left join [dbo].[sqlwatch_meta_sql_query] q
-	--on q.[sqlwatch_query_hash] = e.[sqlwatch_query_hash]
-	--and q.sql_instance = e.sql_instance
+  left join dbo.sqlwatch_meta_query_text qt
+    on qt.sql_instance = e.sql_instance
+    and qt.sqlwatch_query_id = e.sqlwatch_query_id
