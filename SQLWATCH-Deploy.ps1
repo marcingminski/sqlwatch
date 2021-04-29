6c53daafa6b8133259ef37ff7b433e002a3664c3
Marcin Gminski <marcingminski@gmail.com>
2021-04-29 15:22:02 +0100

    tweaks to appveyor tests

--- SQLWATCH-Deploy.ps1
+++ SQLWATCH-Deploy.ps1
@@ -1,16 +1,11 @@
 #PS script to deploy SQLWATCH during appveyor builds
 
-param([string[]]$SqlInstances)
+param([string]$SqlInstance)
 
 # Get root path of this script:
 $PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 
 $DACPAC = Get-ChildItem -Path "$PSScriptRoot\RELEASE\" -Recurse -Filter SQLWATCH.dacpac | Sort-Object LastWriteTime -Descending | Select-Object -First 1
 
-
-ForEach ( $SqlInstance in $SqlInstances ) {
-        sqlpackage.exe /a:Publish /sf:"$($DACPAC.FullName)" /tdn:SQLWATCH /tsn:$SqlInstance
-        if ($LASTEXITCODE -ne 0) {
-                $host.SetShouldExit($LASTEXITCODE)
-                }        
-}
+sqlpackage.exe /a:Publish /sf:"$($DACPAC.FullName)" /tdn:SQLWATCH /tsn:$SqlInstance
+exit $LASTEXITCODE
