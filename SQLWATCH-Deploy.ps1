39fcc2546fc909c9270bd405d1358ff84dd76a11
Marcin Gminski <marcingminski@gmail.com>
2021-04-29 15:16:45 +0100

    fixed exit codes from appveryor tests

--- SQLWATCH-Deploy.ps1
+++ SQLWATCH-Deploy.ps1
@@ -4,11 +4,13 @@ param([string[]]$SqlInstances)
 
 # Get root path of this script:
 $PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
-$PSScriptRoot
 
 $DACPAC = Get-ChildItem -Path "$PSScriptRoot\RELEASE\" -Recurse -Filter SQLWATCH.dacpac | Sort-Object LastWriteTime -Descending | Select-Object -First 1
 
 
 ForEach ( $SqlInstance in $SqlInstances ) {
         sqlpackage.exe /a:Publish /sf:"$($DACPAC.FullName)" /tdn:SQLWATCH /tsn:$SqlInstance
+        if ($LASTEXITCODE -ne 0) {
+                $host.SetShouldExit($LASTEXITCODE)
+                }        
 }
