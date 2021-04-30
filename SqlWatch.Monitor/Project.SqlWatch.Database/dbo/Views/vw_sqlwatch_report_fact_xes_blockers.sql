a9479764464a25d678fce04601f4d653cab51896
Marcin Gminski <marcingminski@gmail.com>
2021-04-30 16:39:05 +0100

    Fix #302. Fixed date parameter

--- SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Views/vw_sqlwatch_report_fact_xes_blockers.sql
+++ SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Views/vw_sqlwatch_report_fact_xes_blockers.sql
@@ -17,4 +17,4 @@ select
 	, [sql_text]
 	, [report_xml]
 	, [sequence]
-from [dbo].[ufn_sqlwatch_get_blocking_chains]('1970-01-01','2099-21-31', null)
+from [dbo].[ufn_sqlwatch_get_blocking_chains]('1970-01-01','2099-12-31', null)
