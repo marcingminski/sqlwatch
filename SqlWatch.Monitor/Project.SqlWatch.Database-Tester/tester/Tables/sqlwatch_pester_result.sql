84b5d4f308b0846434ea331931b7f76a37d53a39
Marcin Gminski <8288333+marcingminski@users.noreply.github.com>
2021-05-04 16:00:30 +0100

    Added separate database for automated tests (#321)

---
+++ SqlWatch.Monitor/Project.SqlWatch.Database-Tester/tester/Tables/sqlwatch_pester_result.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [tester].[sqlwatch_pester_result]
+(
+	[result_datetime_utc] datetime not null constraint df_sqlawatch_pester_result_datetime_utc default getutcdate(),
+	[result_datetime] datetime not null constraint df_sqlawatch_pester_result_datetime_local default getdate(),
+	[Server] [nvarchar](max) NULL,
+	[User] [nvarchar](max) NULL,
+	[DateTime] datetime NULL,
+	[Context] [nvarchar](max) NULL,
+	[TestName] [nvarchar](max) NULL,
+	[TestResult] nvarchar(50) NULL,
+	[TestTime] decimal(20,5) NULL
+);
