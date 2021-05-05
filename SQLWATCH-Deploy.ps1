6359d003b7331154545fb34c98ea6bcae7defb87
Marcin Gminski <marcingminski@gmail.com>
2021-05-05 14:50:21 +0100

    Change to deploy script to be able  to deploy any db

--- SQLWATCH-Deploy.ps1
+++ SQLWATCH-Deploy.ps1
@@ -1,11 +1,12 @@
 #PS script to deploy SQLWATCH during appveyor builds
 
-param([string]$SqlInstance)
+param(
+    [string]$SqlInstance,
+    [string]$Database,
+    [string]$Dacpac
+    )
 
-# Get root path of this script:
-$PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+$DACPAC = Get-ChildItem -Recurse -Filter $Dacpac | Sort-Object LastWriteTime -Descending | Select-Object -First 1
 
-$DACPAC = Get-ChildItem -Path "$PSScriptRoot\RELEASE\" -Recurse -Filter SQLWATCH.dacpac | Sort-Object LastWriteTime -Descending | Select-Object -First 1
-
-sqlpackage.exe /a:Publish /sf:"$($DACPAC.FullName)" /tdn:SQLWATCH /tsn:$SqlInstance
+sqlpackage.exe /a:Publish /sf:"$($DACPAC.FullName)" /tdn:$Database /tsn:$SqlInstance
 exit $LASTEXITCODE
