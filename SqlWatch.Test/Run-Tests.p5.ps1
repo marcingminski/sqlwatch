500ece69d76320f1414588cfed3e168b96041df5
Marcin Gminski <marcingminski@gmail.com>
2021-05-08 20:12:54 +0100

    Workaround for not being able to directly run Invoke-SqlCmd in Appveyor

--- SqlWatch.Test/Run-Tests.p5.ps1
+++ SqlWatch.Test/Run-Tests.p5.ps1
@@ -8,51 +8,6 @@ param(
         [switch]$RunAsJob
 )
 
-##----------------------------------------------------------------------------------------------
-## Prepare environment and tests data
-##----------------------------------------------------------------------------------------------
-
-$sql = "select Hours=datediff(hour,sqlserver_start_time,getdate()) from sys.dm_os_sys_info"
-$SqlUptime = Invoke-SqlCmd -ServerInstance $SqlInstance -Database master -Query $sql
-
-<#
-    Create pester table to store results and other data.
-    This used to be in its own database but there is no need for another databae project.
-    Whilst we do need a separate database to create blocking chains (becuase sqlwatch has RCSI, 
-    we can just create it on the fligh here.
-    The benefit of this approach is that we can just create tables for the purpose of the test in one place here, 
-    rather than having to manage separate database project. The original idea was to move some of the testing stuff
-    from the SQLWATCH database but I will move it all here rather than separate database.
-#>
-
-$sql = "if not exists (select * from sys.databases where name = '$($SqlWatchDatabaseTest)') 
-    begin
-        create database [$($SqlWatchDatabaseTest)]
-    end;
-    ALTER DATABASE [$($SqlWatchDatabaseTest)] SET READ_COMMITTED_SNAPSHOT OFF;
-    ALTER DATABASE [$($SqlWatchDatabaseTest)] SET RECOVERY SIMPLE ;"
-
-Invoke-Sqlcmd -ServerInstance $SqlInstance -Database master -Query $sql
-
-#Create table to store reference data for other tests where required:
-$sql = "if not exists (select * from sys.tables where name = 'sqlwatch_pester_ref')
-begin
-    CREATE TABLE [dbo].[sqlwatch_pester_ref]
-    (
-        [date] datetime NOT NULL,
-        [test] varchar(255) not null,
-    );    
-
-    create index idx_sqlwatch_pester_ref_test
-        on [dbo].[sqlwatch_pester_ref] ([test]) include ([date])
-end;
-
-insert into [dbo].[sqlwatch_pester_ref] (date,test)
-values (getutcdate(),'Test Start')"
-
-Invoke-Sqlcmd -ServerInstance $SqlInstance -Database $SqlWatchDatabaseTest -Query $sql
-
-##----------------------------------------------------------------------------------------------
 
 If ($RunAsJob) {
     $JobName = "Tests@" + $SqlInstance
@@ -86,12 +41,11 @@ If ($RunAsJob) {
                     SqlInstance = $SqlInstance;
                     SqlWatchDatabase = $SqlWatchDatabase;
                     SqlWatchDatabaseTest = "SQLWATCH_TEST";
-                    SqlUptimeHours = $SqlUptimeHours;
                 } )
         )
             
         Invoke-Pester -Configuration $configuration 
-        } -ArgumentList $SqlInstance,$SqlWatchDatabase,$TestFilePath,$ResultsPath,$IncludeTags,$ExcludeTags, $SqlUptime.Hours
+        } -ArgumentList $SqlInstance,$SqlWatchDatabase,$TestFilePath,$ResultsPath,$IncludeTags,$ExcludeTags
     
 } else {
     <#This is repeated and the same as in the Start-Job. I do not know how to make the Pester Configuration generic and Pass into the Job as argument, I am getting:
