3937be39d79ada4566368917f30b672568181d15
Marcin Gminski <marcingminski@gmail.com>
2021-05-08 16:11:45 +0100

    More Pester 5 tests

--- SqlWatch.Test/Run-Tests.p5.ps1
+++ SqlWatch.Test/Run-Tests.p5.ps1
@@ -2,7 +2,9 @@ param(
         [string]$SqlInstance,
         [string]$SqlWatchDatabase,
         [string]$TestFilePath,
-        [string]$ResultsPath
+        [string]$ResultsPath,
+        [string[]]$IncludeTags,
+        [string[]]$ExcludeTags
 )
 
 $TestFile = Get-Item -Path $TestFilePath
@@ -13,6 +15,8 @@ $configuration.TestResult.Enabled = $true
 $configuration.TestResult.OutputPath = $ResultsFile
 $configuration.Output.Verbosity = "Detailed"
 $configuration.CodeCoverage.Enabled = $false
+$configuration.Filter.Tag = $IncludeTags
+$configuration.Filter.ExcludeTag = $ExcludeTags
 $configuration.Run.Exit = $false
 $configuration.Run.Container = @(
     ( New-PesterContainer -Path $($TestFile.FullName) -Data @{ SqlInstance = $SqlInstance; SqlWatchDatabase = $SqlWatchDatabase } )
