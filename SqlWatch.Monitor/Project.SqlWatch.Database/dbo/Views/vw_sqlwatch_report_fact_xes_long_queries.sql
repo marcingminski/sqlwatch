39dc5a9caa69becaddcf77a03f16bd836dfb2d59
Tim armstrong <tima@installernet.com>
2021-05-11 18:17:26 -0400

    Fixed missing query details in report views and Grafana dashboard. (#347)
    * Added query details to report views (#344)
    
    * Create functions to handle the clean/parse of data
    
    * Adding statement termination semicolons
    
    * Adding more semicolons
    
    * Adding new functions to VS project

--- SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Views/vw_sqlwatch_report_fact_xes_long_queries.sql
+++ SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Views/vw_sqlwatch_report_fact_xes_long_queries.sql
@@ -2,38 +2,51 @@
 as
 
 SELECT
-       event_time
-      ,[event_name]
-      ,[session_id]
-      ,[cpu_time]
-      ,[physical_reads]
-      ,[logical_reads]
-      ,[writes]
-      ,[spills]
-      ,[username]
-      ,[client_hostname]
-      ,[client_app_name]
-      ,[duration_ms]
+       d.event_time
+      ,d.[event_name]
+      ,d.[session_id]
+      ,d.[cpu_time]
+      ,d.[physical_reads]
+      ,d.[logical_reads]
+      ,d.[writes]
+      ,d.[spills]
+      ,d.[username]
+      ,d.[client_hostname]
+      ,d.[client_app_name]
+      ,d.[duration_ms]
       ,report_time
       ,d.[sql_instance]
       ,[long_query_id]
- --for backward compatibility with existing pbi, this column will become report_time as we could be aggregating many snapshots in a report_period
-, d.snapshot_time
-, d.snapshot_type_id
-
-    , qp.[query_plan_for_query_plan_hash]
+	  --for backward compatibility with existing pbi, this column will become report_time as we could be aggregating many snapshots in a report_period
+	  , d.snapshot_time
+	  , d.snapshot_type_id
+    , qph.[query_plan_for_query_plan_hash]
+	  , db.[database_name]
+	  , pr.[procedure_name]
+	  , coalesce(dbo.ufn_sqlwatch_clean_sql_text(qph.statement_for_query_plan_hash),ed.sql_text)
+		  as sql_text
   FROM [dbo].[sqlwatch_logger_xes_long_queries] d
   	inner join dbo.sqlwatch_logger_snapshot_header h
 		on  h.snapshot_time = d.[snapshot_time]
 		and h.snapshot_type_id = d.snapshot_type_id
 		and h.sql_instance = d.sql_instance
 
-    left join dbo.[sqlwatch_meta_query_plan] qph
-        on qph.sql_handle = d.sql_instance
-        and qph.plan_handle = d.plan_handle
-        and qph.statement_start_offset = d.statement_start_offset
-        and qph.statement_end_offset = d.statement_end_offset
+    left join dbo.[sqlwatch_meta_query_plan] qp
+        on qp.sql_instance = d.sql_instance
+        and qp.plan_handle = d.plan_handle
+        and qp.statement_start_offset = d.statement_start_offset
+        and qp.statement_end_offset = d.statement_end_offset
 
-    left join dbo.[sqlwatch_meta_query_plan_hash] qp
-        on qp.sql_instance = qph.sql_instance
-        and qp.query_plan_hash = qph.query_plan_hash;
+    left join dbo.[sqlwatch_meta_query_plan_hash] qph
+        on qph.sql_instance = qp.sql_instance
+        and qph.query_plan_hash = qp.query_plan_hash
+
+    left join dbo.[sqlwatch_meta_database] db 
+        on db.sqlwatch_database_id = qp.sqlwatch_database_id
+        and db.sql_instance = qp.sql_instance
+
+    left join dbo.[sqlwatch_meta_procedure] pr
+        on pr.sqlwatch_procedure_id = qp.sqlwatch_procedure_id
+        and pr.sql_instance = qp.sql_instance
+
+	cross apply dbo.ufn_sqlwatch_parse_xes_event_data([event_data]) ed;
