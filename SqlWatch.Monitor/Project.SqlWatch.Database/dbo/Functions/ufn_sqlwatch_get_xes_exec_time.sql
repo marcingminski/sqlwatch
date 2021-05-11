821e02287e938eb315a31805cfddd41d37d0fb60
Marcin Gminski <8288333+marcingminski@users.noreply.github.com>
2021-05-11 18:30:46 +0100

    Improved Performance of XES collectors (#345)
    * Fix #325. Add conditions to all xes collectors to minimise the number of executions and remove code duplication
    
    * Add terminators and created var to store event_date
    
    * Fixed data type and add condition for null time

--- SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Functions/ufn_sqlwatch_get_xes_exec_time.sql
+++ SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Functions/ufn_sqlwatch_get_xes_exec_time.sql
@@ -2,11 +2,12 @@
 (
 	@session_name nvarchar(64)
 )
+-- TO DO TODO CAN BE REMOVED IN vNEXT
 RETURNS datetime2(0) with schemabinding
 AS
 BEGIN
 	RETURN (
-		select [last_updated]
+		select last_event_time
 		from [dbo].[sqlwatch_stage_xes_exec_count]
 		where session_name = @session_name
 	)
--- SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Procedures/usp_sqlwatch_logger_xes_diagnostics.sql
+++ SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Procedures/usp_sqlwatch_logger_xes_diagnostics.sql
@@ -1,72 +1,138 @@
 ﻿CREATE PROCEDURE [dbo].[usp_sqlwatch_logger_xes_diagnostics]
 AS
 
-set xact_abort on
+set xact_abort on;
+set nocount on;
 
-if [dbo].[ufn_sqlwatch_get_product_version]('major') >= 11
+declare @snapshot_type_id tinyint = 7,
+		@snapshot_time datetime2(0),
+		@target_data_char nvarchar(max),
+		@target_data_xml xml,
+		@max_event_time datetime;
+
+declare @execution_count bigint = 0,
+		@session_name nvarchar(64) = 'system_health',
+		@address varbinary(8),
+		@filename varchar(8000),
+		@sql_instance varchar(32) = dbo.ufn_sqlwatch_get_servername(),
+		@store_event_data smallint = dbo.ufn_sqlwatch_get_config_value(23,null),
+		@last_event_time datetime;
+
+declare @event_data utype_event_data;
+
+--bail out if this snapshot is set to not be collected:
+if (select collect 
+	from [dbo].[sqlwatch_config_snapshot_type]
+	where snapshot_type_id = @snapshot_type_id
+	) = 0
 	begin
-		declare @snapshot_time datetime,
-				@snapshot_type_id tinyint = 10,
-				@filename varchar(8000),
-				@target_data_char nvarchar(max),
-				@target_data_xml xml,
-				@utc_offset_minute int = [dbo].[ufn_sqlwatch_get_server_utc_offset]('MINUTE')
+		return;
+	end;
 
-		/* using file target instead of ring buffer to have more resilient source as ring buffer can drop events if there are many */
-		select @filename= [dbo].[ufn_sqlwatch_get_xes_target_file] ('system_health')
+exec [dbo].[usp_sqlwatch_internal_insert_header] 
+	@snapshot_time_new = @snapshot_time OUTPUT,
+	@snapshot_type_id = @snapshot_type_id;
 
-		select cast(event_data as xml) AS target_data
-		into #t
-		from sys.fn_xe_file_target_read_file(@filename, null, null, null) xet
-		where object_name = 'sp_server_diagnostics_component_result'
+begin transaction;
 
-		exec [dbo].[usp_sqlwatch_internal_insert_header] 
-			@snapshot_time_new = @snapshot_time OUTPUT,
-			@snapshot_type_id = @snapshot_type_id
+	insert into @event_data
+	exec [dbo].[usp_sqlwatch_internal_get_xes_data]
+		@session_name = @session_name,
+		@object_name = 'sp_server_diagnostics_component_result',
+		@min_interval_s = 300;
 
-		begin tran
+	--bail out of no xes data to process:
+	if not exists (select top 1 * from @event_data)
+		begin
+			commit transaction;
+			return;
+		end;
 
-			insert into [dbo].[sqlwatch_logger_xes_query_processing](event_time, max_workers, workers_created, idle_workers, pending_tasks
-				, unresolvable_deadlocks, deadlocked_scheduler, snapshot_time, snapshot_type_id
-			)
-			select 
-				[event_time] = dateadd(minute,@utc_offset_minute,xml_node.value('(./@timestamp)[1]','datetime')),
-				[max_workers] = report_xml_node.value('(./@maxWorkers)[1]','bigint'),
-				[workers_created] = report_xml_node.value('(./@workersCreated)[1]','bigint'),
-				[idle_workers] = report_xml_node.value('(./@workersIdle)[1]','bigint'),
-				[pending_tasks] = report_xml_node.value('(./@pendingTasks)[1]','bigint'),
-				[unresolvable_deadlocks] = report_xml_node.value('(./@hasUnresolvableDeadlockOccurred)[1]','int'),
-				[deadlocked_scheduler] = report_xml_node.value('(./@hasDeadlockedSchedulersOccurred)[1]','int'),
-				[snapshot_time] = @snapshot_time,
-				[snapshot_type_id] = @snapshot_type_id
-			--from sys.fn_xe_file_target_read_file(@filename, null, null, null) xet
-			from #t xet
-			cross apply ( select cast(xet.target_data as xml) ) AS target_data ([xml])
-			cross apply target_data.[xml].nodes('/event[@name="sp_server_diagnostics_component_result"]') AS xml_nodes(xml_node)
-			cross apply xml_node.nodes('./data[@name="data"]/value/queryProcessing') AS report_xml_nodes(report_xml_node)
-			where xml_node.value('(./@timestamp)[1]','datetime') > (select isnull(max(event_time),'1970-01-01') from [dbo].[sqlwatch_logger_xes_query_processing])
-			--and xet.object_name = 'sp_server_diagnostics_component_result'
-			option (maxdop 1);
+	select @max_event_time = max(event_time) 
+	from [dbo].[sqlwatch_logger_xes_query_processing]
+	where sql_instance = @sql_instance;
 
-			insert into [dbo].[sqlwatch_logger_xes_iosubsystem](event_time, io_latch_timeouts, total_long_ios, longest_pending_request_file, longest_pending_request_duration,
-				snapshot_time, snapshot_type_id
-			)
-			select
-				[event_time] = xml_node.value('(./@timestamp)[1]','datetime'),
-				[io_latch_timeouts] = report_xml_node.value('(./@ioLatchTimeouts)[1]','bigint'),
-				[total_long_ios] = report_xml_node.value('(./@totalLongIos)[1]','bigint'),
-				[longest_pending_request_file] = report_xml_node.value('(./longestPendingRequests/pendingRequest/@filePath)[1]','varchar(255)'),
-				[longest_pending_request_duration] = report_xml_node.value('(./longestPendingRequests/pendingRequest/@duration)[1]','bigint'),
-				[snapshot_time] = @snapshot_time,
-				[snapshot_type_id] = @snapshot_type_id
-			--from sys.fn_xe_file_target_read_file(@filename, null, null, null) xet
-			from #t xet
-			cross apply ( select cast(xet.target_data as xml) ) AS target_data ([xml])
-			cross apply target_data.[xml].nodes('/event[@name="sp_server_diagnostics_component_result"]') AS xml_nodes(xml_node)
-			cross apply xml_node.nodes('./data[@name="data"]/value/ioSubsystem') AS report_xml_nodes(report_xml_node)
-			where xml_node.value('(./@timestamp)[1]','datetime') > (select isnull(max(event_time),'1970-01-01') from [dbo].[sqlwatch_logger_xes_iosubsystem])
-			--and xet.object_name = 'sp_server_diagnostics_component_result'
-			option (maxdop 1);
+	set @max_event_time = isnull(@max_event_time,'1970-01-01');
 
-		commit tran
-	end
+	with cte_query_processing as (
+		select 
+			[event_time] =  xed.event_data.value('(@timestamp)[1]', 'datetime'),
+			[max_workers] = report_xml_node.value('(./@maxWorkers)[1]','bigint'),
+			[workers_created] = report_xml_node.value('(./@workersCreated)[1]','bigint'),
+			[idle_workers] = report_xml_node.value('(./@workersIdle)[1]','bigint'),
+			[pending_tasks] = report_xml_node.value('(./@pendingTasks)[1]','bigint'),
+			[unresolvable_deadlocks] = report_xml_node.value('(./@hasUnresolvableDeadlockOccurred)[1]','int'),
+			[deadlocked_scheduler] = report_xml_node.value('(./@hasDeadlockedSchedulersOccurred)[1]','int'),
+			[snapshot_time] = @snapshot_time,
+			[snapshot_type_id] = @snapshot_type_id
+		from @event_data t
+		cross apply t.event_data.nodes('event') as xed (event_data)
+		cross apply xed.event_data.nodes('./data[@name="data"]/value/queryProcessing') AS report_xml_nodes(report_xml_node)
+	)
+	insert into [dbo].[sqlwatch_logger_xes_query_processing](
+			event_time
+		, max_workers
+		, workers_created
+		, idle_workers
+		, pending_tasks
+		, unresolvable_deadlocks
+		, deadlocked_scheduler
+		, snapshot_time
+		, snapshot_type_id
+		)
+	select 
+		  [event_time]
+		, [max_workers]
+		, [workers_created]
+		, [idle_workers]
+		, [pending_tasks]
+		, [unresolvable_deadlocks]
+		, [deadlocked_scheduler]
+		, [snapshot_time]
+		, [snapshot_type_id]
+	from cte_query_processing
+	where event_time > @max_event_time
+	option (maxdop 1, keepfixed plan);
+
+	select @max_event_time = max(event_time) 
+	from [dbo].[sqlwatch_logger_xes_iosubsystem]
+	where sql_instance = @sql_instance;
+
+	set @max_event_time = isnull(@max_event_time,'1970-01-01');
+
+	with cte_io_subsystem as (
+		select
+			[event_time] = xed.event_data.value('(@timestamp)[1]', 'datetime'),
+			[io_latch_timeouts] = report_xml_node.value('(./@ioLatchTimeouts)[1]','bigint'),
+			[total_long_ios] = report_xml_node.value('(./@totalLongIos)[1]','bigint'),
+			[longest_pending_request_file] = report_xml_node.value('(./longestPendingRequests/pendingRequest/@filePath)[1]','varchar(255)'),
+			[longest_pending_request_duration] = report_xml_node.value('(./longestPendingRequests/pendingRequest/@duration)[1]','bigint'),
+			[snapshot_time] = @snapshot_time,
+			[snapshot_type_id] = @snapshot_type_id
+		from @event_data t
+		cross apply t.event_data.nodes('event') as xed (event_data)
+		cross apply xed.event_data.nodes('./data[@name="data"]/value/ioSubsystem') AS report_xml_nodes(report_xml_node)
+	)
+
+	insert into [dbo].[sqlwatch_logger_xes_iosubsystem](
+		  event_time
+		, io_latch_timeouts
+		, total_long_ios
+		, longest_pending_request_file
+		, longest_pending_request_duration
+		, snapshot_time
+		, snapshot_type_id
+		)
+	select 
+		  [event_time]
+		, [io_latch_timeouts]
+		, [total_long_ios]
+		, [longest_pending_request_file]
+		, [longest_pending_request_duration]
+		, [snapshot_time]
+		, [snapshot_type_id] 
+	from cte_io_subsystem
+	where event_time > @max_event_time
+	option (maxdop 1, keepfixed plan);
+
+commit transaction;
