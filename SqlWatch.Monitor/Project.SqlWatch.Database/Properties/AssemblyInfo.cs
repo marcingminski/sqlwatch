b0b93b8522307edc642c50466add73cb1e9b072e
Marcin Gminski <8288333+marcingminski@users.noreply.github.com>
2021-05-11 16:08:54 +0100

    Version bump to 4.2 (#340)

--- SqlWatch.Monitor/Project.SqlWatch.Database/Properties/AssemblyInfo.cs
+++ SqlWatch.Monitor/Project.SqlWatch.Database/Properties/AssemblyInfo.cs
@@ -19,5 +19,5 @@
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.1.0.*")]
-[assembly: AssemblyFileVersion("4.1.0.0")]
+[assembly: AssemblyVersion("4.2.0.*")]
+[assembly: AssemblyFileVersion("4.2.0.0")]
