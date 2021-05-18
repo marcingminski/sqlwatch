d70e56235db6d893d4aa4f7fce7fa5ebbabcf96b
Marcin Gminski <8288333+marcingminski@users.noreply.github.com>
2021-05-18 13:00:05 +0100

    Fixed Wait Events Grafana Dashboard (#365)
    * Removed plan affecting convert as this is captured by query problems.
    * Exclude IntelliSense
    * Fixed cartesian product and code formatting
    * Add query plan hash to Grafana for filtering

--- SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Views/vw_sqlwatch_report_fact_xes_wait_events.sql
+++ SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Views/vw_sqlwatch_report_fact_xes_wait_events.sql
@@ -18,15 +18,15 @@ select e.[event_time]
       ,e.statement_start_offset
       ,e.statement_end_offset
       ,qph.[query_plan_for_query_plan_hash]
-      , db.[database_name]
-	  , pr.[procedure_name]
-	  , coalesce(dbo.ufn_sqlwatch_clean_sql_text(qph.statement_for_query_plan_hash),ed.sql_text)
-		  as sql_text
+      ,db.[database_name]
+	  ,pr.[procedure_name]
+	  ,sql_text =  coalesce(dbo.ufn_sqlwatch_clean_sql_text(qph.statement_for_query_plan_hash),ed.sql_text)
+      ,qp.query_plan_hash
   from [dbo].[sqlwatch_logger_xes_wait_event] e
 
-  inner join dbo.vw_sqlwatch_meta_wait_stats_category ws
-	on e.[wait_type_id] = ws.[wait_type_id]
-	and e.sql_instance = ws.sql_instance
+    inner join dbo.vw_sqlwatch_meta_wait_stats_category ws
+	    on e.[wait_type_id] = ws.[wait_type_id]
+	    and e.sql_instance = ws.sql_instance
 
     left join dbo.[sqlwatch_meta_query_plan] qp
         on qp.sql_instance = e.sql_instance
@@ -36,7 +36,7 @@ select e.[event_time]
 
     left join dbo.[sqlwatch_meta_query_plan_hash] qph
         on qph.sql_instance = qp.sql_instance
-        and qph.query_plan_hash = qph.query_plan_hash
+        and qph.query_plan_hash = qp.query_plan_hash
         
     left join dbo.[sqlwatch_meta_database] db 
         on db.sqlwatch_database_id = qp.sqlwatch_database_id
@@ -46,5 +46,4 @@ select e.[event_time]
         on pr.sqlwatch_procedure_id = qp.sqlwatch_procedure_id
         and pr.sql_instance = qp.sql_instance
 			
-	cross apply dbo.ufn_sqlwatch_parse_xes_event_data([event_data]) ed;
-
+    cross apply dbo.ufn_sqlwatch_parse_xes_event_data([event_data]) ed;
