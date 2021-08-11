a75b3de8a5cc29de93a6a585e76208f6c5a97d02
kekcjkee <47787046+kekcjkee@users.noreply.github.com>
2021-08-11 14:00:22 +0300

    Predicate for filtering non-deleted records #410

--- SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Views/vw_sqlwatch_report_dim_agent_job.sql
+++ SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Views/vw_sqlwatch_report_dim_agent_job.sql
@@ -2,3 +2,4 @@
 	AS 
 	select [sql_instance], [job_name], [job_create_date], [sqlwatch_job_id], [date_last_seen] , [is_record_deleted]
 	from dbo.sqlwatch_meta_agent_job
+	where [is_record_deleted] = 0 or [is_record_deleted] is null
