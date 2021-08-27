740481af7a2660288fcfd335644305c8c8e69b5d
Marcin Gminski <marcingminski@gmail.com>
2021-08-27 16:55:31 +0100

    Remove CLR related objects

--- SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Functions/ufn_sqlwatch_get_clr_collector_status.sql
+++ SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Functions/ufn_sqlwatch_get_clr_collector_status.sql
@@ -1,13 +1,13 @@
-﻿CREATE FUNCTION [dbo].[ufn_sqlwatch_get_clr_collector_status]
-()
-RETURNS bit 
-AS
-BEGIN
-	RETURN (select 
-		case 
-			when dbo.ufn_sqlwatch_get_config_value (21, null) = 1
-			and dbo.ufn_sqlwatch_get_clr_status() = 1 
-			then 1 
-		else 0 
-		end)
-END
+﻿--CREATE FUNCTION [dbo].[ufn_sqlwatch_get_clr_collector_status]
+--()
+--RETURNS bit 
+--AS
+--BEGIN
+--	RETURN (select 
+--		case 
+--			when dbo.ufn_sqlwatch_get_config_value (21, null) = 1
+--			and dbo.ufn_sqlwatch_get_clr_status() = 1 
+--			then 1 
+--		else 0 
+--		end)
+--END
--- SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Functions/ufn_sqlwatch_get_clr_status.sql
+++ SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Functions/ufn_sqlwatch_get_clr_status.sql
@@ -1,7 +1,7 @@
-﻿CREATE FUNCTION [dbo].[ufn_sqlwatch_get_clr_status]
-()
-RETURNS bit
-AS
-BEGIN
-	RETURN (select convert(bit,value_in_use) from sys.configurations where name = 'clr enabled')
-END
+﻿--CREATE FUNCTION [dbo].[ufn_sqlwatch_get_clr_status]
+--()
+--RETURNS bit
+--AS
+--BEGIN
+--	RETURN (select convert(bit,value_in_use) from sys.configurations where name = 'clr enabled')
+--END
