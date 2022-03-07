02c58cbcfe6350a013c99867a787ffe73dcf88ca
Dick Pluim <67138922+pluim003@users.noreply.github.com>
2022-03-07 10:20:24 +0100

    Corrected syntax
    Seemed the 'create-script' from SSMS messed up some things. This syntax is better.

--- SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Tables/sqlwatch_logger_xes_iosubsystem.sql
+++ SqlWatch.Monitor/Project.SqlWatch.Database/dbo/Tables/sqlwatch_logger_xes_iosubsystem.sql
@@ -18,6 +18,5 @@
 go
 
 create unique nonclustered index idx_sqlwatch_xes_iosubsystem_event_time
-	on [dbo].[sqlwatch_logger_xes_iosubsystem] ([event_time]),
-	   [dbo].[sqlwatch_logger_xes_iosubsystem] ([sql_instance]);
+	on [dbo].[sqlwatch_logger_xes_iosubsystem] ([event_time], [sql_instance]);
 go
