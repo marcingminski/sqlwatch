0ecd73c9ace53244b0a97c04eb30b9dee2e5901d
Marcin Gminski <8288333+marcingminski@users.noreply.github.com>
2022-04-02 00:56:39 +0100

    Suppress 7zip output

--- AppVeyor/Build.ps1
+++ AppVeyor/Build.ps1
@@ -1,7 +1,9 @@
 $job = Start-Job -Name TestSpace -ScriptBlock { 
     cd c:\projects\sqlwatch\SqlWatch.Test
     Start-FileDownload https://testspace-client.s3.amazonaws.com/testspace-windows.zip 
-    7z x -y testspace-windows.zip
+    Write-Output "Extracting archive..."
+    7z x -y testspace-windows.zip -bso0 -bsp0 
+
     }
     
     $ErrorActionPreference = "Stop"
