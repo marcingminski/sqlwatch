5ba34441d46b783d15c75e6c2f9161d653d2cae6
Marcin Gminski <8288333+marcingminski@users.noreply.github.com>
2022-04-02 15:54:21 +0100

    Appveyor init script

---
+++ AppVeyor/Init.ps1
@@ -0,0 +1,16 @@
+#Set all installed instances of SQL server to dynamic ports
+Get-ChildItem -Path 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\' |
+    Where-Object {
+        $_.Name -imatch 'MSSQL[_\d]+\.SQL.*'
+    } |
+    ForEach-Object {
+
+        Write-Host "Setting $((Get-ItemProperty $_.PSPath).'(default)') to dynamic ports"
+        Set-ItemProperty (Join-Path $_.PSPath 'mssqlserver\supersocketnetlib\tcp\ipall') -Name TcpDynamicPorts -Value '0'
+        Set-ItemProperty (Join-Path $_.PSPath 'mssqlserver\supersocketnetlib\tcp\ipall') -Name TcpPort -Value ([string]::Empty)
+    }
+
+#Install Modules
+Start-Job -Name GetPester -ScriptBlock { powershell -command Install-Module Pester -RequiredVersion 5.2.0 -Force -SkipPublisherCheck -Scope CurrentUser }
+Start-Job -Name GetDbaTools -ScriptBlock { powershell -command Install-Module dbatools -Force -SkipPublisherCheck }
+Start-Job -Name GetDbaChecks -ScriptBlock { powershell -command Install-Module dbachecks -Force -SkipPublisherCheck -Scope CurrentUser }
