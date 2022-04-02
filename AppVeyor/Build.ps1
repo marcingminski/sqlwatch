dc5121a909fc24115992667f1f1e69166a96b975
Marcin Gminski <8288333+marcingminski@users.noreply.github.com>
2022-04-02 16:41:33 +0100

    removed carriage return

--- AppVeyor/Build.ps1
+++ AppVeyor/Build.ps1
@@ -6,21 +6,21 @@ $TmpFolder = "$ProjectFolder\RELEASE\"
 $ReleaseFolderName = "SQLWATCH Latest"
 $ReleaseFolder = "$TmpFolder\$ReleaseFolderName"
 
-Write-Output "`nCreating the Release folder..."
+Write-Output "Creating the Release folder..."
 if (Test-Path -path $TmpFolder) 
 {
     Remove-Item -Path $TmpFolder -Force -Confirm:$false -Recurse | Out-Null
 }
 New-Item -Path $ReleaseFolder -ItemType Directory | Out-Null
 
-Write-Output "`nRestoring NuGet packages..." 
+Write-Output "Restoring NuGet packages..." 
 nuget restore "$ProjectFolder\SqlWatch.Monitor\SqlWatch.Monitor.sln"  -Verbosity quiet
 if ($LASTEXITCODE -ne 0) 
 {
     exit $LASTEXITCODE
 }
 
-Write-Output "`nBuilding Database Project"
+Write-Output "Building Database Project..."
 MSBuild.exe /m -v:m -nologo "$ProjectFolder\SqlWatch.Monitor\Project.SqlWatch.Database\SQLWATCH.sqlproj" /clp:ErrorsOnly /p:Configuration=Release /p:Platform="Any CPU" /p:OutDir="$($ReleaseFolder)"
 if ($LASTEXITCODE -ne 0) 
 {
