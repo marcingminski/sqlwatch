82b5e368009ce9d503f93e52243b46da30252175
Marcin Gminski <8288333+marcingminski@users.noreply.github.com>
2022-04-02 16:25:11 +0100

    Add Start SqlServer as Job

--- AppVeyor/Install.ps1
+++ AppVeyor/Install.ps1
@@ -13,12 +13,16 @@ Get-ChildItem -Path 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\' |
     }
 
 #Install Modules
-Start-Job -Name GetPester -ScriptBlock { powershell -command Install-Module Pester -RequiredVersion 5.2.0 -Force -SkipPublisherCheck -Scope CurrentUser }
-Start-Job -Name GetDbaTools -ScriptBlock { powershell -command Install-Module dbatools -Force -SkipPublisherCheck }
-Start-Job -Name GetDbaChecks -ScriptBlock { powershell -command Install-Module dbachecks -Force -SkipPublisherCheck -Scope CurrentUser }
-
-#Start SQL Server
-Get-Service | Where-Object {$_.DisplayName -like 'SQL Server (*'} | Start-Service
+Start-Job -Name GetPester -ScriptBlock { Install-Module Pester -RequiredVersion 5.2.0 -Force -SkipPublisherCheck -Scope CurrentUser }
+Start-Job -Name GetDbaTools -ScriptBlock { Install-Module dbatools -Force -SkipPublisherCheck }
+Start-Job -Name GetDbaChecks -ScriptBlock { Install-Module dbachecks -Force -SkipPublisherCheck -Scope CurrentUser }
+Start-Job -Name TestSpace -ScriptBlock { 
+    cd c:\projects\sqlwatch\SqlWatch.Test
+    Start-FileDownload https://testspace-client.s3.amazonaws.com/testspace-windows.zip 
+    Write-Output "Extracting archive..."
+    7z x -y testspace-windows.zip -bso0 -bsp0 
+    }
+Start-Job -Name StartSqlServer -ScriptBlock { Get-Service | Where-Object {$_.DisplayName -like 'SQL Server (*'} | Start-Service }
 
 #Wait for the jobs to finish
 Get-Job | Wait-Job | Receive-Job | Format-Table
